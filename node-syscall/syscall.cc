2c131993d55b9ec1cdb333c80f64fd4971739bee
Richard Musiol <mail@richard-musiol.de>
2013-10-02 17:20:27 +0200

    Made pipe and exec syscalls work.

--- node-syscall/syscall.cc
+++ node-syscall/syscall.cc
@@ -1,31 +1,63 @@
 #include <node.h>
 #include <v8.h>
 #include <unistd.h>
+#include <iostream>
 
 using namespace v8;
 
-size_t toNative(v8::Local<v8::Value> value) {
+#define SYS_FORK 2
+#define SYS_PIPE 42
+
+const char* ABC = "abc";
+
+size_t toNative(Local<Value> value) {
   if (value->IsArrayBufferView()) {
     Local<ArrayBufferView> view = Local<ArrayBufferView>::Cast(value);
-    return size_t(view->BaseAddress());
+    return (size_t)view->BaseAddress();
+  }
+  if (value->IsArray()) {
+    Local<Array> array = Local<Array>::Cast(value);
+    size_t* native = (size_t*)malloc(array->Length() * sizeof(size_t)); // TODO memory leak
+    for (uint32_t i = 0; i < array->Length() - 1; i++) {
+      native[i] = toNative(array->CloneElementAt(i));
+    }
+    native[array->Length() - 1] = 0;
+    return (size_t)native;
   }
   return value->ToInteger()->Value();
 }
 
 void Syscall(const FunctionCallbackInfo<Value>& info) {
-  int r = syscall(
-    info[0]->ToInteger()->Value(),
-    toNative(info[1]),
-    toNative(info[2]),
-    toNative(info[3])
-  );
+  int trap = info[0]->ToInteger()->Value();
+  int r1 = 0, r2 = 0;
+  switch (trap) {
+  case SYS_FORK:
+    r1 = fork();
+    break;
+  case SYS_PIPE:
+    int fd[2];
+    r1 = pipe(fd);
+    if (r1 == 0) {
+      r1 = fd[0];
+      r2 = fd[1];
+    }
+    break;
+  default:
+    r1 = syscall(
+      trap,
+      toNative(info[1]),
+      toNative(info[2]),
+      toNative(info[3])
+    );
+    break;
+  }
   int err = 0;
-  if (r < 0) {
+  if (r1 < 0) {
     err = errno;
   }
   Local<Array> res = Array::New(3);
-  res->Set(0, Integer::New(r));
-  res->Set(1, Integer::New(0));
+  res->Set(0, Integer::New(r1));
+  res->Set(1, Integer::New(r2));
   res->Set(2, Integer::New(err));
   info.GetReturnValue().Set(res);
 }

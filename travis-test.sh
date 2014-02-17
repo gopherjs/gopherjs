5b6b32a86e505989d371b77534ae38b64a2ab03b
Richard Musiol <mail@richard-musiol.de>
2014-02-17 23:45:25 +0100

    travis: trying to use node_js config

--- travis-test.sh
+++ travis-test.sh
@@ -1,9 +1,9 @@
 #! /bin/sh
 set -e
 
-sudo apt-get install rlwrap
-wget https://launchpad.net/~rwky/+archive/nodejs-unstable/+files/nodejs_0.11.11-rwky1~precise_amd64.deb
-sudo dpkg -i nodejs_0.11.11-rwky1~precise_amd64.deb
+# sudo apt-get install rlwrap
+# wget https://launchpad.net/~rwky/+archive/nodejs-unstable/+files/nodejs_0.11.11-rwky1~precise_amd64.deb
+# sudo dpkg -i nodejs_0.11.11-rwky1~precise_amd64.deb
 
 which node
 node -v

54a5eea5cb9aa769f5e51240ccd1b0c079f5101d
Richard Musiol <mail@richard-musiol.de>
2014-02-18 00:08:06 +0100

    travis: use bash and dot

--- travis-test.sh
+++ travis-test.sh
@@ -1,10 +1,11 @@
-#! /bin/sh
+#! /bin/bash
 set -e
 
 # sudo apt-get install rlwrap
 # wget https://launchpad.net/~rwky/+archive/nodejs-unstable/+files/nodejs_0.11.11-rwky1~precise_amd64.deb
 # sudo dpkg -i nodejs_0.11.11-rwky1~precise_amd64.deb
 
+echo "running nvm"
 . $HOME/.nvm/nvm.sh
 nvm install 0.11
 nvm use 0.11

392bff9ac695ec564a6e2fbf5a9755cf7ec81024
Richard Musiol <mail@richard-musiol.de>
2014-02-17 23:38:00 +0100

    install node.js 0.11.11 on travis

--- test.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/sh
-set -e
-
-# TODO
---
+++ travis-test.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+set -e
+
+sudo apt-get install rlwrap
+wget https://launchpad.net/~rwky/+archive/nodejs-unstable/+files/nodejs_0.11.11-rwky1~precise_amd64.deb
+sudo dpkg -i nodejs_0.11.11-rwky1~precise_amd64.deb
+
+node -v

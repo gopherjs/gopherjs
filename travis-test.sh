f09f9af4541c8c86b5256e3cf616eba6a1b350df
Richard Musiol <mail@richard-musiol.de>
2014-02-18 00:32:35 +0100

    travis: build and install syscall module

--- travis-test.sh
+++ travis-test.sh
@@ -1,18 +1,14 @@
 #! /bin/bash
 set -e
 
-# sudo apt-get install rlwrap
-# wget https://launchpad.net/~rwky/+archive/nodejs-unstable/+files/nodejs_0.11.11-rwky1~precise_amd64.deb
-# sudo dpkg -i nodejs_0.11.11-rwky1~precise_amd64.deb
-
-echo "get fresh nvm"
 curl https://raw.github.com/creationix/nvm/master/install-gitless.sh | bash
-echo "loading nvm"
 . $HOME/.nvm/nvm.sh
-echo "installing node"
 nvm install 0.11
-echo "using node"
 nvm use 0.11
 
-which node
-node -v
+cd node-syscall
+npm install --global node-gyp
+node-gyp rebuild
+mkdir -p ~/.node_libraries/
+cp build/Release/syscall.node ~/.node_libraries/syscall.node
+cd ..

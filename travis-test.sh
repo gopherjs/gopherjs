188c47c5d9ff4fa22e66ce061730de1103aa550b
Richard Musiol <mail@richard-musiol.de>
2014-02-18 00:24:37 +0100

    travis: get fresh nvm

--- travis-test.sh
+++ travis-test.sh
@@ -5,6 +5,8 @@ set -e
 # wget https://launchpad.net/~rwky/+archive/nodejs-unstable/+files/nodejs_0.11.11-rwky1~precise_amd64.deb
 # sudo dpkg -i nodejs_0.11.11-rwky1~precise_amd64.deb
 
+echo "get fresh nvm"
+curl https://raw.github.com/creationix/nvm/master/install.sh | sh
 echo "loading nvm"
 . $HOME/.nvm/nvm.sh
 echo "installing node"

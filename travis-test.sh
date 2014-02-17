46839ee11b650d65ee5620976379f3ca4b3652ca
Richard Musiol <mail@richard-musiol.de>
2014-02-18 00:33:08 +0100

    travis: build and install syscall module

--- travis-test.sh
+++ travis-test.sh
@@ -6,8 +6,8 @@ curl https://raw.github.com/creationix/nvm/master/install-gitless.sh | bash
 nvm install 0.11
 nvm use 0.11
 
-cd node-syscall
 npm install --global node-gyp
+cd node-syscall
 node-gyp rebuild
 mkdir -p ~/.node_libraries/
 cp build/Release/syscall.node ~/.node_libraries/syscall.node

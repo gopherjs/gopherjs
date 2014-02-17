4f5d86037d25ae3f169528863dc2abe99a54d972
Richard Musiol <mail@richard-musiol.de>
2014-02-17 23:42:23 +0100

    travis: which node

--- travis-test.sh
+++ travis-test.sh
@@ -5,4 +5,5 @@ sudo apt-get install rlwrap
 wget https://launchpad.net/~rwky/+archive/nodejs-unstable/+files/nodejs_0.11.11-rwky1~precise_amd64.deb
 sudo dpkg -i nodejs_0.11.11-rwky1~precise_amd64.deb
 
+which node
 node -v

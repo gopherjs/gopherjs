00794ea1bdeede2c105a1e6a92f03a6081fb781a
Richard Musiol <mail@richard-musiol.de>
2014-02-17 23:48:17 +0100

    travis: nvm use 0.11

--- travis-test.sh
+++ travis-test.sh
@@ -5,5 +5,8 @@ set -e
 # wget https://launchpad.net/~rwky/+archive/nodejs-unstable/+files/nodejs_0.11.11-rwky1~precise_amd64.deb
 # sudo dpkg -i nodejs_0.11.11-rwky1~precise_amd64.deb
 
+nvm install 0.11
+nvm use 0.11
+
 which node
 node -v

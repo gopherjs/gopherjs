3cdd4bcc262f92b8ed72122ffbcf3b3725238ebc
Richard Musiol <mail@richard-musiol.de>
2014-02-18 01:33:06 +0100

    travis: silent npm install and command echo

--- travis-test.sh
+++ travis-test.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
-set -e
+set -xe
 
 curl https://raw.github.com/creationix/nvm/master/install-gitless.sh | bash
 . $HOME/.nvm/nvm.sh
 nvm install 0.11
 nvm use 0.11
 
-npm install --global node-gyp
+npm install --silent --global node-gyp
 
 cd node-syscall
 node-gyp rebuild

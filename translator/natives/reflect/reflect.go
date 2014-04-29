9bc2355eb4db988d59fff1200041ef8f4ad141a8
Richard Musiol <mail@richard-musiol.de>
2014-04-29 13:32:02 +0200

    translated reflect.call to Go (one commit left)

--- translator/natives/reflect/reflect.go
+++ translator/natives/reflect/reflect.go
@@ -503,6 +503,99 @@ func (v Value) iword() iword {
 	return iword(v.val)
 }
 
+func (v Value) call(op string, in []Value) []Value {
+	t := v.typ
+	var (
+		fn   unsafe.Pointer
+		rcvr iword
+	)
+	if v.flag&flagMethod != 0 {
+		t, fn, rcvr = methodReceiver(op, v, int(v.flag)>>flagMethodShift)
+	} else {
+		fn = unsafe.Pointer(v.iword())
+	}
+
+	if fn == nil {
+		panic("reflect.Value.Call: call of nil function")
+	}
+
+	isSlice := op == "CallSlice"
+	n := t.NumIn()
+	if isSlice {
+		if !t.IsVariadic() {
+			panic("reflect: CallSlice of non-variadic function")
+		}
+		if len(in) < n {
+			panic("reflect: CallSlice with too few input arguments")
+		}
+		if len(in) > n {
+			panic("reflect: CallSlice with too many input arguments")
+		}
+	} else {
+		if t.IsVariadic() {
+			n--
+		}
+		if len(in) < n {
+			panic("reflect: Call with too few input arguments")
+		}
+		if !t.IsVariadic() && len(in) > n {
+			panic("reflect: Call with too many input arguments")
+		}
+	}
+	for _, x := range in {
+		if x.Kind() == Invalid {
+			panic("reflect: " + op + " using zero Value argument")
+		}
+	}
+	for i := 0; i < n; i++ {
+		if xt, targ := in[i].Type(), t.In(i); !xt.AssignableTo(targ) {
+			panic("reflect: " + op + " using " + xt.String() + " as type " + targ.String())
+		}
+	}
+	if !isSlice && t.IsVariadic() {
+		// prepare slice for remaining values
+		m := len(in) - n
+		slice := MakeSlice(t.In(n), m, m)
+		elem := t.In(n).Elem()
+		for i := 0; i < m; i++ {
+			x := in[n+i]
+			if xt := x.Type(); !xt.AssignableTo(elem) {
+				panic("reflect: cannot use " + xt.String() + " as type " + elem.String() + " in " + op)
+			}
+			slice.Index(i).Set(x)
+		}
+		origIn := in
+		in = make([]Value, n+1)
+		copy(in[:n], origIn)
+		in[n] = slice
+	}
+
+	nin := len(in)
+	if nin != t.NumIn() {
+		panic("reflect.Value.Call: wrong argument count")
+	}
+	nout := t.NumOut()
+
+	argsArray := js.Global.Get("Array").New(t.NumIn())
+	for i, arg := range in {
+		argsArray.SetIndex(i, arg.assignTo("reflect.Value.Call", t.In(i).common(), nil).iword())
+	}
+	results := js.InternalObject(fn).Call("apply", rcvr, argsArray)
+
+	switch nout {
+	case 0:
+		return nil
+	case 1:
+		return []Value{makeValue(t.Out(0), results, 0)}
+	default:
+		ret := make([]Value, nout)
+		for i := range ret {
+			ret[i] = makeValue(t.Out(i), results.Index(i), 0)
+		}
+		return ret
+	}
+}
+
 func (v Value) Cap() int {
 	k := v.kind()
 	switch k {

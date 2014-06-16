a8bb86f7528ee63954d5fe79f0e2f138ee9ff9e9
Richard Musiol <mail@richard-musiol.de>
2014-06-16 12:00:38 +0200

    os.Args needs to have at least a command name in the browser

--- compiler/natives/os/os.go
+++ compiler/natives/os/os.go
@@ -8,11 +8,13 @@ import (
 
 func init() {
 	process := js.Global.Get("process")
-	if !process.IsUndefined() {
-		args := process.Get("argv")
-		Args = make([]string, args.Length()-1)
-		for i := 0; i < args.Length()-1; i++ {
-			Args[i] = args.Index(i + 1).Str()
-		}
+	if process.IsUndefined() {
+		Args = []string{"browser"}
+		return
+	}
+	args := process.Get("argv")
+	Args = make([]string, args.Length()-1)
+	for i := 0; i < args.Length()-1; i++ {
+		Args[i] = args.Index(i + 1).Str()
 	}
 }

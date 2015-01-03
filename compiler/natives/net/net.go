86e6d623af3981c414a5e12046048d2dc5915de0
Richard Musiol <mail@richard-musiol.de>
2015-01-03 20:53:12 +0100

    diable net package initialization (#123)

--- compiler/natives/net/net.go
+++ compiler/natives/net/net.go
@@ -9,3 +9,14 @@ import (
 func Listen(net, laddr string) (Listener, error) {
 	panic(&runtime.NotSupportedError{"net"})
 }
+
+func sysInit() {
+}
+
+func probeIPv4Stack() bool {
+	return false
+}
+
+func probeIPv6Stack() (supportsIPv6, supportsIPv4map bool) {
+	return false, false
+}

3c8ab9ebc9eeac7cdc181e6c0939136a4b5a1252
Richard Musiol <mail@richard-musiol.de>
2015-01-11 18:28:09 +0100

    Revert "determine global object via this in context of new function (#153)"
    This reverts commit 635a153b2f382fb394a1975f8196b6ca40477950.

--- compiler/prelude/prelude.go
+++ compiler/prelude/prelude.go
@@ -4,7 +4,17 @@ const Prelude = prelude + types + numeric + goroutines + jsmapping
 
 const prelude = `Error.stackTraceLimit = Infinity;
 
-var $global = (function() { return this; })();
+var $global, $module;
+if (typeof window !== "undefined") { /* web page */
+  $global = window;
+} else if (typeof self !== "undefined") { /* web worker */
+  $global = self;
+} else if (typeof global !== "undefined") { /* Node.js */
+  $global = global;
+  $global.require = require;
+} else {
+  console.log("warning: no global object found");
+}
 if (typeof module !== "undefined") {
   $module = module;
 }

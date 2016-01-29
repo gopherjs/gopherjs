e9089e636ec1071da4f18af15e6231d205cfddf2
Anders Brander <anders@brander.dk>
2016-01-29 23:09:27 +0100

    Override os.Hostname()
    os.Hostname() doesn't work in compiled code because it will trigger a syscall. This replacement tries to do the right thing (tm).

--- compiler/natives/os/os.go
+++ compiler/natives/os/os.go
@@ -24,3 +24,27 @@ func init() {
 }
 
 func runtime_beforeExit() {}
+
+// Override the builtin os.Hostname(). The builtin Hostname() will result in
+// a non-working syscall on most (all?) platforms
+func Hostname() (string, error) {
+	// Browser
+	// The browser doesn't know anything about any hostname, so we use the host part of
+	// the current url.
+	if location := js.Global.Get("location"); location != js.Undefined {
+		if browserHostname := location.Get("hostname"); browserHostname != js.Undefined {
+			return browserHostname.String(), nil
+		}
+	}
+
+	// Node.js
+	// We call node's require("os").hostname() which should return current hostname
+	if require := js.Global.Get("require"); require != js.Undefined {
+		// "os" is in nodejs core, it should always exist
+		if nodeHostname := require.Invoke("os").Call("hostname"); nodeHostname != js.Undefined {
+			return nodeHostname.String(), nil
+		}
+	}
+
+	return "", ErrNotExist
+}

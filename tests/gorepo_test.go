aee41f98e41e352d73205976f1404f4c881434cf
Dmitri Shuralyov <shurcooL@gmail.com>
2016-04-30 16:16:22 -0700

    circle: Require source-map-support for tests.
    This fixes one known failing test from Go compiler test suite.

--- tests/gorepo_test.go
+++ tests/gorepo_test.go
@@ -5,11 +5,19 @@ package tests_test
 import (
 	"os"
 	"os/exec"
+	"strconv"
 	"testing"
 )
 
 // Go repository basic compiler tests, and regression tests for fixed compiler bugs.
 func TestGoRepositoryCompilerTests(t *testing.T) {
+	if b, _ := strconv.ParseBool(os.Getenv("SOURCE_MAP_SUPPORT")); os.Getenv("SOURCE_MAP_SUPPORT") != "" && !b {
+		t.Fatal("Source maps disabled, but required for this test. Use SOURCE_MAP_SUPPORT=true or unset it completely.")
+	}
+	if err := exec.Command("node", "--require", "source-map-support/register", "--eval", "").Run(); err != nil {
+		t.Fatal("Source maps disabled, but required for this test. Use Node.js 4.x with source-map-support module for nice stack traces.")
+	}
+
 	args := []string{"go", "run", "run.go", "-summary"}
 	if testing.Verbose() {
 		args = append(args, "-v")

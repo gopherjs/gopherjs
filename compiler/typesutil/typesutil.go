f6275185df124b006416e1be1c791b000619867f
Richard Musiol <mail@richard-musiol.de>
2016-05-15 00:40:30 +0200

    detect js package by /gopherjs/js suffix
    This should allow vendoring. (#415)

--- compiler/typesutil/typesutil.go
+++ compiler/typesutil/typesutil.go
@@ -1,9 +1,12 @@
 package typesutil
 
-import "go/types"
+import (
+	"go/types"
+	"strings"
+)
 
 func IsJsPackage(pkg *types.Package) bool {
-	return pkg != nil && pkg.Path() == "github.com/gopherjs/gopherjs/js"
+	return pkg != nil && strings.HasSuffix(pkg.Path(), "/gopherjs/js")
 }
 
 func IsJsObject(t types.Type) bool {

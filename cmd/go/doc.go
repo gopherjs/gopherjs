2d17f70a2d766667d46ef6431a1bea8dbe5d2ff1
Dmitri Shuralyov <shurcooL@gmail.com>
2016-06-27 01:45:30 -0700

    WIP: Use cmd/go with GopherJS support.
    Very alpha, much unfinished. Dragons be here.
    
    It won't work on your computer yet, unless you're me. But that will be
    fixed soon, up next.
    
    TODO: Split into logical commits, document and explain changes.
    
    Update.
    
    https://github.com/gopherjs/go/commit/524b8a70b6b558603ceca9144d36e7113f0d37b9
    
    Use own go/build replacement.
    
    Update.
    
    https://github.com/gopherjs/go/commit/4254365d55f5a069526e73d904ba1b04f4e52709
    
    WIP: Use embedded js, nosync packages.
    
    For now, try to use pre-built js.a and nosync.a until we can figure out
    a good way to build them in a normal way. Try to do this in order to
    make progress in CI and see what next steps need to be taken.
    
    Don't use own go/build replacement for importgraph.
    
    It's needed for GopherJS compiler, but we can't use it for importgraph
    package since they're 3rd party and rely on "go/build".Package.
    
    WIP: Try a fix for issue 10332.
    
    WIP: Work on disabling cgo, filling custom files during test package builds.

---
+++ cmd/go/doc.go
@@ -0,0 +1,5 @@
+// Go is a tool for managing Go source code.
+//
+// This is a feature branch of the official cmd/go command with
+// GopherJS compiler support added (via -compiler=gopherjs flag).
+package main

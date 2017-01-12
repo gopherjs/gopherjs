42e645943c55c7a36ccd46f479b8ee038fe51b2e
Dmitri Shuralyov <shurcooL@gmail.com>
2017-01-12 18:26:19 -0500

    compiler/typesutil: Detect vendored js package and exit.
    This follows #538.
    
    Vendoring github.com/gopherjs/gopherjs/js package is not supported, and can cause
    hard to understand runtime errors. Better catch it earlier and print a clear error message.
    
    Helps #569.

--- compiler/typesutil/typesutil.go
+++ compiler/typesutil/typesutil.go
@@ -1,11 +1,19 @@
 package typesutil
 
 import (
+	"fmt"
 	"go/types"
+	"os"
 	"strings"
 )
 
 func IsJsPackage(pkg *types.Package) bool {
+	// TODO: Resolve issue #415 and remove this temporary workaround.
+	if pkg != nil && strings.HasSuffix(pkg.Path(), "/vendor/github.com/gopherjs/gopherjs/js") {
+		fmt.Fprintln(os.Stderr, "GopherJS: vendoring github.com/gopherjs/gopherjs/js package is not supported, see https://github.com/gopherjs/gopherjs/issues/415.")
+		os.Exit(1)
+	}
+
 	return pkg != nil && (pkg.Path() == "github.com/gopherjs/gopherjs/js" || strings.HasSuffix(pkg.Path(), "/vendor/github.com/gopherjs/gopherjs/js"))
 }
 

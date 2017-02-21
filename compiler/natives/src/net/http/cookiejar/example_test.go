c44e315799ad076a3b439b9cb62cbadb0496bc2b
Jonathan Hall <flimzy@flimzy.com>
2017-02-21 20:24:50 +0100

    Disable two Examples not supported by GopherJS

---
+++ compiler/natives/src/net/http/cookiejar/example_test.go
@@ -0,0 +1,7 @@
+// +build js
+
+package cookiejar
+
+func ExampleNew() {
+  // network access not supported by GopherJS, and this test depends on httptest.NewServer
+}
---
+++ compiler/natives/src/reflect/example_test.go
@@ -0,0 +1,8 @@
+// +build js
+
+package reflect
+
+func ExampleStructOf() {
+  // GopherJS does not implement reflect.addReflectOff needed for this test.
+  // See https://github.com/gopherjs/gopherjs/issues/499
+}

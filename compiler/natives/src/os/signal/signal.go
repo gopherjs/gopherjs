981eddd8f261d7467858b10235f692e01e3d25f4
Dmitri Shuralyov <shurcooL@gmail.com>
2017-06-23 21:08:22 -0400

    compiler/natives/src/os/signal: Create no-op implementation.
    Package testing now imports os/signal as of Go 1.9, which it didn't before.
    A no-op implementation fixes the build.
    
    Fixes:
    
    	Error: runtime error: native function not implemented: os/signal.signal_enable

---
+++ compiler/natives/src/os/signal/signal.go
@@ -0,0 +1,13 @@
+// +build js
+
+package signal
+
+// Package signal is not implemented for GOARCH=js.
+// TODO: Implement if possible.
+
+func signal_disable(uint32) {}
+func signal_enable(uint32)  {}
+func signal_ignore(uint32)  {}
+func signal_recv() uint32   { return 0 }
+
+func loop() {}

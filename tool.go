7698c319bdf28d1fed5c3b5621d130825de7b421
Nate <nathan@4ydx.com>
2017-09-27 16:20:50 +0900

    Simply simple determination of the proper object build name.

--- tool.go
+++ tool.go
@@ -143,11 +143,7 @@ func main() {
 					}
 					if len(pkgs) == 1 { // Only consider writing output if single package specified.
 						if pkgObj == "" {
-							if !build.IsLocalImport(pkg.ImportPath) {
-								pkgObj = filepath.Base(pkg.ImportPath) + ".js"
-							} else {
-								pkgObj = filepath.Base(pkg.Dir) + ".js"
-							}
+							pkgObj = filepath.Base(pkg.Dir) + ".js"
 						}
 						if pkg.IsCommand() && !pkg.UpToDate {
 							if err := s.WriteCommandPackage(archive, pkgObj); err != nil {

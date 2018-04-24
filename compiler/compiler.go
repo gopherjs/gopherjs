423bf76ba1888a53d4fe3c1a82991cdb019a52ad
Paul Jolly <paul@myitcv.io>
2018-04-24 21:23:57 +0100

    compiler: Use gcexportdata instead of gcimporter15. (#803)
    This change updates compiler to use new package gcexportdata instead of
    the deprecated and removed package gcimporter15 for reading and writing
    export data files containing type information.
    
    See its deprecation note here:
    
    https://github.com/golang/tools/blob/14d5b80f954f510ba9bcd241821b6f4828498e1a/go/gcimporter15/gcimporter.go#L13-L14
    
    Remove vendored copy of gcimporter15, since it's no longer used.
    
    Updates #789.

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -12,7 +12,7 @@ import (
 	"strings"
 
 	"github.com/gopherjs/gopherjs/compiler/prelude"
-	"golang.org/x/tools/go/gcimporter15"
+	"golang.org/x/tools/go/gcexportdata"
 )
 
 var sizes32 = &types.StdSizes{WordSize: 4, MaxAlign: 8}
@@ -243,7 +243,7 @@ func ReadArchive(filename, path string, r io.Reader, packages map[string]*types.
 	}
 
 	var err error
-	_, packages[path], err = gcimporter.BImportData(token.NewFileSet(), packages, a.ExportData, path)
+	packages[path], err = gcexportdata.Read(bytes.NewReader(a.ExportData), token.NewFileSet(), packages, path)
 	if err != nil {
 		return nil, err
 	}

cafa873c3d5760cc25485f771bdb40c943f8c188
Paul Jolly <paul@myitcv.io>
2018-04-30 07:31:37 +0100

    Add a couple more tests for varidic nil/non-nil checks

--- tests/compiler_test.go
+++ tests/compiler_test.go
@@ -12,4 +12,18 @@ func TestVariadicNil(t *testing.T) {
 	if got := printVari(); got != nil {
 		t.Errorf("printVari(): got: %#v; want %#v.", got, nil)
 	}
+
+	{
+		var want []string
+		if got := printVari(want...); got != nil {
+			t.Errorf("printVari(want...): got: %#v; want %#v.", got, nil)
+		}
+	}
+
+	{
+		want := []string{}
+		if got := printVari(want...); got == nil || len(got) != len(want) {
+			t.Errorf("printVari(want...): got: %#v; want %#v.", got, want)
+		}
+	}
 }

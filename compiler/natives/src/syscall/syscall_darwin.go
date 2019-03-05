35239cfae03b95edd83d98b2403bd1528db6da34
Hajime Hoshi <hajimehoshi@gmail.com>
2019-03-05 23:49:04 +0900

    Fix testing issues on macOS

--- compiler/natives/src/syscall/syscall_darwin.go
+++ compiler/natives/src/syscall/syscall_darwin.go
@@ -2,31 +2,33 @@
 
 package syscall
 
-func funcPC(func()) uintptr {
-	return 0
+import (
+	"github.com/gopherjs/gopherjs/js"
+)
+
+func funcPC(f func()) uintptr {
+	if js.InternalObject(f) == js.InternalObject(libc_write_trampoline) {
+		return SYS_WRITE
+	}
+	return uintptr(minusOne)
 }
 
 func syscall(trap, a1, a2, a3 uintptr) (r1, r2 uintptr, err Errno) {
-	printWarning()
-	return uintptr(minusOne), 0, EACCES
+	return Syscall(trap, a1, a2, a3)
 }
 
 func syscall6(trap, a1, a2, a3, a4, a5, a6 uintptr) (r1, r2 uintptr, err Errno) {
-	printWarning()
-	return uintptr(minusOne), 0, EACCES
+	return Syscall6(trap, a1, a2, a3, a4, a5, a6)
 }
 
 func syscall6X(trap, a1, a2, a3, a4, a5, a6 uintptr) (r1, r2 uintptr, err Errno) {
-	printWarning()
-	return uintptr(minusOne), 0, EACCES
+	panic("syscall6X is not implemented")
 }
 
 func rawSyscall(trap, a1, a2, a3 uintptr) (r1, r2 uintptr, err Errno) {
-	printWarning()
-	return uintptr(minusOne), 0, EACCES
+	return RawSyscall(trap, a1, a2, a3)
 }
 
 func rawSyscall6(trap, a1, a2, a3, a4, a5, a6 uintptr) (r1, r2 uintptr, err Errno) {
-	printWarning()
-	return uintptr(minusOne), 0, EACCES
+	return RawSyscall6(trap, a1, a2, a3, a4, a5, a6)
 }

cf2e4059f76a442caeed3f661953e9cf3633a9da
Dmitri Shuralyov <dmitri@shuralyov.com>
2019-07-24 17:05:58 -0700

    target Go 1.13 Beta 1, update version to GopherJS 1.13-wip
    Reference: https://groups.google.com/d/msg/golang-announce/fmU5r5NfHZs/xU3Ym5KJCAAJ.
    
    Updates #929

--- compiler/version_check.go
+++ compiler/version_check.go
@@ -1,9 +1,9 @@
-// +build go1.12
-// +build !go1.13
+// +build go1.13
+// +build !go1.14
 
 package compiler
 
-const ___GOPHERJS_REQUIRES_GO_VERSION_1_12___ = true
+const ___GOPHERJS_REQUIRES_GO_VERSION_1_13___ = true
 
 // Version is the GopherJS compiler version string.
-const Version = "1.12-2"
+const Version = "1.13-wip"

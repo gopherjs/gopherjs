d3ddacdb130fcd23f77a827e3b599804730be6b5
Dmitri Shuralyov <dmitri@shuralyov.com>
2019-09-15 15:48:58 -0400

    compiler/natives/src/syscall: support more Darwin syscalls
    Go 1.12 has changed syscall on Darwin to start using libSystem for
    forward-compatibility. That means we need to add support for more
    signals to be able to run all supported tests and examples on macOS.
    
    Many of the mappings from libc_aaa_trampoline to SYS_AAA were based on
    https://github.com/golang/go/commit/a3b01440fef3d2833909f6651455924a1c86d192.
    
    Improve error when an unsupported signal is called,
    by making the signal name visible in the error message.
    
    Regenerate natives:
    
    	go generate github.com/gopherjs/gopherjs/compiler/natives
    
    GitHub-Pull-Request: #940

--- compiler/natives/src/syscall/syscall_darwin.go
+++ compiler/natives/src/syscall/syscall_darwin.go
@@ -2,18 +2,67 @@
 
 package syscall
 
-import (
-	"github.com/gopherjs/gopherjs/js"
-)
+import "github.com/gopherjs/gopherjs/js"
 
 func funcPC(f func()) uintptr {
-	if js.InternalObject(f) == js.InternalObject(libc_write_trampoline) {
+	switch js.InternalObject(f) {
+	case js.InternalObject(libc_open_trampoline):
+		return SYS_OPEN
+	case js.InternalObject(libc_stat64_trampoline):
+		return SYS_STAT64
+	case js.InternalObject(libc_fstat64_trampoline):
+		return SYS_FSTAT64
+	case js.InternalObject(libc_lstat64_trampoline):
+		return SYS_LSTAT64
+	case js.InternalObject(libc_mkdir_trampoline):
+		return SYS_MKDIR
+	case js.InternalObject(libc_chdir_trampoline):
+		return SYS_CHDIR
+	case js.InternalObject(libc_rmdir_trampoline):
+		return SYS_RMDIR
+	case js.InternalObject(libc___getdirentries64_trampoline):
+		return SYS_GETDIRENTRIES64
+	case js.InternalObject(libc_getattrlist_trampoline):
+		return SYS_GETATTRLIST
+	case js.InternalObject(libc_symlink_trampoline):
+		return SYS_SYMLINK
+	case js.InternalObject(libc_readlink_trampoline):
+		return SYS_READLINK
+	case js.InternalObject(libc_fcntl_trampoline):
+		return SYS_FCNTL
+	case js.InternalObject(libc_read_trampoline):
+		return SYS_READ
+	case js.InternalObject(libc_pread_trampoline):
+		return SYS_PREAD
+	case js.InternalObject(libc_write_trampoline):
 		return SYS_WRITE
+	case js.InternalObject(libc_lseek_trampoline):
+		return SYS_LSEEK
+	case js.InternalObject(libc_close_trampoline):
+		return SYS_CLOSE
+	case js.InternalObject(libc_unlink_trampoline):
+		return SYS_UNLINK
+	case js.InternalObject(libc_getpid_trampoline):
+		return SYS_GETPID
+	case js.InternalObject(libc_getuid_trampoline):
+		return SYS_GETUID
+	case js.InternalObject(libc_getgid_trampoline):
+		return SYS_GETGID
+	default:
+		// If we just return -1, the caller can only print an unhelpful generic error message, like
+		// "signal: bad system call".
+		// So, execute f() to get a more helpful error message that includes the syscall name, like
+		// "runtime error: native function not implemented: syscall.libc_getpid_trampoline".
+		f()
+		return uintptr(minusOne)
 	}
-	return uintptr(minusOne)
 }
 
 func syscall(trap, a1, a2, a3 uintptr) (r1, r2 uintptr, err Errno) {
+	return Syscall(trap, a1, a2, a3)
+}
+
+func syscallX(trap, a1, a2, a3 uintptr) (r1, r2 uintptr, err Errno) {
 	return Syscall(trap, a1, a2, a3)
 }
 

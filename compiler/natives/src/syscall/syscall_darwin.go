2386bbb93ff3533776abe05d8f347248af7fc4df
Dmitri Shuralyov <dmitri@shuralyov.com>
2019-09-14 22:15:23 -0400

    compiler/natives/src/syscall: define more syscall functions for Darwin
    These are needed to be able to execute examples.
    After this change, 'gopherjs test reflect' passes successfully.
    
    Improve error when an unsupported signal is called,
    by making the signal name visible in the error message.
    
    Regenerate natives:
    
    	go generate github.com/gopherjs/gopherjs/compiler/natives

--- compiler/natives/src/syscall/syscall_darwin.go
+++ compiler/natives/src/syscall/syscall_darwin.go
@@ -7,10 +7,29 @@ import (
 )
 
 func funcPC(f func()) uintptr {
-	if js.InternalObject(f) == js.InternalObject(libc_write_trampoline) {
+	switch js.InternalObject(f) {
+	case js.InternalObject(libc_open_trampoline):
+		return SYS_OPEN
+	case js.InternalObject(libc_fstat64_trampoline):
+		return SYS_FSTAT64
+	case js.InternalObject(libc_read_trampoline):
+		return SYS_READ
+	case js.InternalObject(libc_write_trampoline):
 		return SYS_WRITE
+	case js.InternalObject(libc_close_trampoline):
+		return SYS_CLOSE
+	case js.InternalObject(libc_unlink_trampoline):
+		return SYS_UNLINK
+	case js.InternalObject(libc_getpid_trampoline):
+		return SYS_GETPID
+	default:
+		// If we just return -1, the caller can only print an unhelpful generic error message, like
+		// "signal: bad system call".
+		// So, execute f() to get a more helpful error message that includes the syscall name, like
+		// "runtime error: native function not implemented: syscall.libc_getpid_trampoline".
+		f()
+		return uintptr(minusOne)
 	}
-	return uintptr(minusOne)
 }
 
 func syscall(trap, a1, a2, a3 uintptr) (r1, r2 uintptr, err Errno) {

f40b3922ae976b52d7f6f1329c391657e9961f35
visualfc <visualfc@gmail.com>
2020-02-09 10:42:43 +0800

    version_check: support Go 1.12 1.13 both

--- compiler/go113_version_check.go
+++ compiler/go113_version_check.go
@@ -1,9 +1,8 @@
-// +build go1.12
-// +build !go1.13
+// +build go1.13
 
 package compiler
 
 const ___GOPHERJS_REQUIRES_GO_VERSION_1_12___ = true
 
 // Version is the GopherJS compiler version string.
-const Version = "1.12-2"
+const Version = "1.13-wip"

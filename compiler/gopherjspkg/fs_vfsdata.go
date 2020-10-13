1fc237a234e2e030f24be78053c44553b95fad6b
visualfc <visualfc@gmail.com>
2020-10-13 11:47:08 +0800

    go generate

--- compiler/gopherjspkg/fs_vfsdata.go
+++ compiler/gopherjspkg/fs_vfsdata.go
@@ -21,7 +21,7 @@ var FS = func() http.FileSystem {
 	fs := vfsgen۰FS{
 		"/": &vfsgen۰DirInfo{
 			name:    "/",
-			modTime: time.Date(2020, 6, 3, 15, 38, 46, 484467445, time.UTC),
+			modTime: time.Date(2020, 10, 13, 0, 23, 9, 811949088, time.UTC),
 		},
 		"/js": &vfsgen۰DirInfo{
 			name:    "js",

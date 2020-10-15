11eab9b964a6e71c95ccf80c047eee9fe918733c
visualfc <visualfc@gmail.com>
2020-10-15 08:29:38 +0800

    tests: update Go1.15 fixedbugs

--- tests/run.go
+++ tests/run.go
@@ -136,8 +136,6 @@ var knownFails = map[string]failReason{
 	"fixedbugs/issue32680.go":  {desc: "unknown shorthand flag"},
 
 	// These are new test in Go 1.15
-	"fixedbugs/issue16130.go": {desc: "got *js.Error, expected runtime.Error"},
-	"fixedbugs/issue26094.go": {desc: "TypeError: Cannot read property 'ptr' of undefined"},
 	"fixedbugs/issue34395.go": {desc: "FATAL ERROR: CALL_AND_RETRY_LAST Allocation failed - JavaScript heap out of memory"},
 	"fixedbugs/issue35027.go": {desc: "unknown shorthand flag: 'g' in -gcflags=-d=checkptr"},
 	"fixedbugs/issue35073.go": {desc: "unknown shorthand flag: 'g' in -gcflags=-d=checkptr"},

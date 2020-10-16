7f2e97b1fea5941dbd2c53e30ca239fd64dd90fb
visualfc <visualfc@gmail.com>
2020-10-16 22:46:04 +0800

    compileer/natives/src: fix GOOS=linux

--- compiler/natives/src/internal/syscall/unix/unix.go
+++ compiler/natives/src/internal/syscall/unix/unix.go
@@ -2,8 +2,12 @@
 
 package unix
 
-const randomTrap = 0
-const fstatatTrap = 0
+const (
+	randomTrap                = 0
+	fstatatTrap               = 0
+	getrandomTrap     uintptr = 0
+	copyFileRangeTrap uintptr = 0
+)
 
 func IsNonblock(fd int) (nonblocking bool, err error) {
 	return false, nil

e7305add8c12d4594b3ee69f4dfe5a6c7e2c9954
visualfc <visualfc@gmail.com>
2020-10-29 23:15:22 +0800

    compiler/natives/src/math/big: skip slow test

--- compiler/natives/src/math/big/big_test.go
+++ compiler/natives/src/math/big/big_test.go
@@ -11,3 +11,11 @@ func TestBytes(t *testing.T) {
 func TestModSqrt(t *testing.T) {
 	t.Skip("slow")
 }
+
+func TetSqr(t *testing.T) {
+	t.Skip("slow")
+}
+
+func TestNatDiv(t *testing.T) {
+	t.Skip("slow")
+}

2f51d53aa2fa015688e97d7458dd845a1d427120
visualfc <visualfc@gmail.com>
2021-02-19 14:23:33 +0800

    compiler/natives/src/runtime: merge fastrand

--- compiler/natives/src/runtime/runtime.go
+++ compiler/natives/src/runtime/runtime.go
@@ -245,3 +245,30 @@ func findfunc(pc uintptr) funcInfo { return funcInfo{} }
 func funcname(f funcInfo) string   { return "" }
 
 type funcInfo struct{}
+
+func rand32() uint32 {
+	return uint32(js.Global.Get("Math").Call("random").Float() * (1<<32 - 1))
+}
+
+var (
+	fastrand0 uint32
+	fastrand1 uint32
+)
+
+func init() {
+	fastrand0 = rand32()
+	fastrand1 = rand32()
+}
+
+func fastrand() uint32 {
+	// Implement xorshift64+: 2 32-bit xorshift sequences added together.
+	// Shift triplet [17,7,16] was calculated as indicated in Marsaglia's
+	// Xorshift paper: https://www.jstatsoft.org/article/view/v008i14/xorshift.pdf
+	// This generator passes the SmallCrush suite, part of TestU01 framework:
+	// http://simul.iro.umontreal.ca/testu01/tu01.html
+	s1, s0 := fastrand0, fastrand1
+	s1 ^= s1 << 17
+	s1 = s1 ^ s0 ^ s1>>7 ^ s0>>16
+	fastrand0, fastrand1 = s0, s1
+	return s0 + s1
+}

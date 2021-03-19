gopherjs/gopherjs

commit e1112ef8f0d0a28e7694606a7b71d729d08a12bb

Support compiling Go 1.16 `runtime` with GopherJS. (#990)
Support compiling Go 1.16 `runtime` with GopherJS.

The vast majority of the code (even in errors.go) is not used by
GopherJS, so starting with this commit the package is entirely defined
by our own implementation in natives. As a bonus, this allowed to drop
a few unexported symbols that existed purely to let other unused
functions to compile.

compiler/natives/src/runtime/runtime.go
@@ -12,19 +12,23 @@ const GOOS = sys.GOOS
 const GOARCH = "js"
 const Compiler = "gopherjs"
 
-// fake for error.go
-type eface struct {
-	_type *_type
-}
-type _type struct {
-	str string
+// The Error interface identifies a run time error.
+type Error interface {
+	error
+
+	// RuntimeError is a no-op function but
+	// serves to distinguish types that are run time
+	// errors from ordinary errors: a type is a
+	// run time error if it has a RuntimeError method.
+	RuntimeError()
 }
 
-func (t *_type) string() string {
-	return t.str
-}
-func (t *_type) pkgpath() string {
-	return ""
+// A TypeAssertionError explains a failed type assertion.
+type TypeAssertionError struct{}
+
+func (*TypeAssertionError) RuntimeError() {}
+func (*TypeAssertionError) Error() string {
+	panic("TypeAssertionError is not used in GopherJS.")
 }
 
 func init() {
@@ -54,9 +58,7 @@ func GOROOT() string {
 	return "/usr/local/go"
 }
 
-func Breakpoint() {
-	js.Debugger()
-}
+func Breakpoint() { js.Debugger() }
 
 func Caller(skip int) (pc uintptr, file string, line int, ok bool) {
 	info := js.Global.Get("Error").New().Get("stack").Call("split", "\n").Index(skip + 2)
@@ -88,17 +90,14 @@ type Frame struct {
 	Entry    uintptr
 }
 
-func GC() {
-}
+func GC() {}
 
 func Goexit() {
 	js.Global.Get("$curGoroutine").Set("exit", true)
 	js.Global.Call("$throw", nil)
 }
 
-func GOMAXPROCS(n int) int {
-	return 1
-}
+func GOMAXPROCS(int) int { return 1 }
 
 func Gosched() {
 	c := make(chan struct{})
@@ -106,9 +105,7 @@ func Gosched() {
 	<-c
 }
 
-func NumCPU() int {
-	return 1
-}
+func NumCPU() int { return 1 }
 
 func NumGoroutine() int {
 	return js.Global.Get("$totalGoroutines").Int()
@@ -165,9 +162,13 @@ type MemStats struct {
 }
 
 func ReadMemStats(m *MemStats) {
+	// TODO(nevkontakte): This function is effectively unimplemented and may
+	// lead to silent unexpected behaviors. Consider panicing explicitly.
 }
 
 func SetFinalizer(x, f interface{}) {
+	// TODO(nevkontakte): This function is effectively unimplemented and may
+	// lead to silent unexpected behaviors. Consider panicing explicitly.
 }
 
 type Func struct {
@@ -217,20 +218,8 @@ func NumCgoCall() int64 {
 	return 0
 }
 
-func efaceOf(ep *interface{}) *eface {
-	panic("efaceOf: not supported")
-}
-
 func KeepAlive(interface{}) {}
 
 func throw(s string) {
-	panic(errorString(s))
+	panic("runtime error: " + s)
 }
-
-// These are used by panicwrap. Not implemented for GOARCH=js.
-// TODO: Implement if possible.
-func getcallerpc() uintptr         { return 0 }
-func findfunc(pc uintptr) funcInfo { return funcInfo{} }
-func funcname(f funcInfo) string   { return "" }
-
-type funcInfo struct{}

build/build.go
@@ -176,7 +176,7 @@ func importWithSrcDir(bctx build.Context, path string, srcDir string, mode build
 	case "os":
 		pkg.GoFiles = excludeExecutable(pkg.GoFiles) // Need to exclude executable implementation files, because some of them contain package scope variables that perform (indirectly) syscalls on init.
 	case "runtime":
-		pkg.GoFiles = []string{"error.go"}
+		pkg.GoFiles = []string{} // Package sources are completely replaced in natives.
 	case "runtime/internal/sys":
 		pkg.GoFiles = []string{fmt.Sprintf("zgoos_%s.go", bctx.GOOS), "zversion.go"}
 	case "runtime/pprof":

compiler/natives/src/internal/cpu/cpu.go
@@ -6,3 +6,5 @@ const (
 	CacheLineSize    = 0
 	CacheLinePadSize = 0
 )
+
+func doinit() {}

commit 5bcafdbfc8d00da206bff533637613832c65a51a

Restore implementation of TypeAssertionError in `runtime`.
This is another regression introduced in e1112ef8, this error is
actually used in prelude's $assertType function, along with a fake
`_type` struct.

Admittedly, this isn't the best fix, but reconciling type information
with the reflect types is a rabbit hole I'm unwilling to jump into right
now.

compiler/natives/src/runtime/runtime.go
@@ -23,12 +23,48 @@ type Error interface {
 	RuntimeError()
 }
 
+// TODO(nevkontakte): In the upstream, this struct is meant to be compatible
+// with reflect.rtype, but here we use a minimal stub that satisfies the API
+// TypeAssertionError expects, which we dynamically instantiate in $assertType().
+type _type struct{ str string }
+
+func (t *_type) string() string  { return t.str }
+func (t *_type) pkgpath() string { return "" }
+
 // A TypeAssertionError explains a failed type assertion.
-type TypeAssertionError struct{}
+type TypeAssertionError struct {
+	_interface    *_type
+	concrete      *_type
+	asserted      *_type
+	missingMethod string // one method needed by Interface, missing from Concrete
+}
 
 func (*TypeAssertionError) RuntimeError() {}
-func (*TypeAssertionError) Error() string {
-	panic("TypeAssertionError is not used in GopherJS.")
+
+func (e *TypeAssertionError) Error() string {
+	inter := "interface"
+	if e._interface != nil {
+		inter = e._interface.string()
+	}
+	as := e.asserted.string()
+	if e.concrete == nil {
+		return "interface conversion: " + inter + " is nil, not " + as
+	}
+	cs := e.concrete.string()
+	if e.missingMethod == "" {
+		msg := "interface conversion: " + inter + " is " + cs + ", not " + as
+		if cs == as {
+			// provide slightly clearer error message
+			if e.concrete.pkgpath() != e.asserted.pkgpath() {
+				msg += " (types from different packages)"
+			} else {
+				msg += " (types from different scopes)"
+			}
+		}
+		return msg
+	}
+	return "interface conversion: " + cs + " is not " + as +
+		": missing method " + e.missingMethod
 }
 
 func init() {

tests/misc_test.go
@@ -683,7 +683,7 @@ func TestInterfaceConversionRuntimeError(t *testing.T) {
 		}
 		re, ok := r.(runtime.Error)
 		if !ok {
-			t.Fatalf("got %T, want runtime.Error", r)
+			t.Fatalf("got %T (%s), want runtime.Error", r, r)
 		}
 		if got, want := re.Error(), "interface conversion: int is not tests.I: missing method Get"; got != want {
 			t.Fatalf("got %q, want %q", got, want)
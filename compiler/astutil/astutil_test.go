gopherjs/gopherjs

commit 58c1058cb3db0d3a6d0303037051f1db057e29a9

Prune replaced function bodies.
Functions replaced by a GopherJS overlay may contain invalid coed (e.g.
assuming a 64-bit architecture) and may still prevent the augmented code
from compiling.

Since this may make certain imports unused, we prune them as well.

build/build.go
@@ -24,6 +24,7 @@ import (
 	"github.com/gopherjs/gopherjs/compiler/natives"
 	"github.com/neelance/sourcemap"
 	"github.com/shurcooL/httpfs/vfsutil"
+	"golang.org/x/tools/go/ast/astutil"
 	"golang.org/x/tools/go/buildutil"
 )
 
@@ -447,6 +448,9 @@ func parseAndAugment(bctx *build.Context, pkg *build.Package, isTest bool, fileS
 			case *ast.FuncDecl:
 				if replacedDeclNames[funcName(d)] {
 					d.Name = ast.NewIdent("_")
+					// Prune function bodies, since it may contain code invalid for
+					// GopherJS and pin unwanted imports.
+					d.Body = nil
 				}
 			case *ast.GenDecl:
 				switch d.Tok {
@@ -469,8 +473,24 @@ func parseAndAugment(bctx *build.Context, pkg *build.Package, isTest bool, fileS
 				}
 			}
 		}
+
+		// Prune any imports that might have become unused after pruning function bodies.
+		for _, impGroup := range astutil.Imports(fileSet, file) {
+			for _, imp := range impGroup {
+				path, _ := strconv.Unquote(imp.Path.Value)
+				if !astutil.UsesImport(file, path) {
+					name := ""
+					if imp.Name != nil {
+						name = imp.Name.Name
+					}
+					astutil.DeleteNamedImport(fileSet, file, name, path)
+				}
+			}
+		}
+
 		files = append(files, file)
 	}
+
 	if errList != nil {
 		return nil, errList
 	}

commit b123b5bd1376173a66e72657ea7a9e74a029f259

Add a compiler directive to control function body pruning.
Fixed https://github.com/gopherjs/gopherjs/issues/1033, which was caused
by incorrect import pruning, which was introduced in
https://github.com/gopherjs/gopherjs/commit/58c1058cb3db0d3a6d0303037051f1db057e29a9.

The original intent was to prune all function bodies, which were
replaced by the corresponding overlay. That, in turn, required pruning
imports which might have become unused in the modified AST tree.
Unfortunately, checking whether the import is used requires knowing the
imported package name, which may not match the last component of the
import path and is not readily available at package augmentation stage.

This change drops import pruning and introduces the
`gopherjs:prune-original` directive to only trigger function body
pruning selectively to prevent compile errors when it doesn't lead to
unused imports. When it does, a different solution is required, like on
the `TestGarbageCollection` case.

build/build.go
@@ -20,11 +20,11 @@ import (
 
 	"github.com/fsnotify/fsnotify"
 	"github.com/gopherjs/gopherjs/compiler"
+	"github.com/gopherjs/gopherjs/compiler/astutil"
 	"github.com/gopherjs/gopherjs/compiler/gopherjspkg"
 	"github.com/gopherjs/gopherjs/compiler/natives"
 	"github.com/neelance/sourcemap"
 	"github.com/shurcooL/httpfs/vfsutil"
-	"golang.org/x/tools/go/ast/astutil"
 	"golang.org/x/tools/go/buildutil"
 )
 
@@ -301,16 +301,8 @@ func ImportDir(dir string, mode build.ImportMode, installSuffix string, buildTag
 func parseAndAugment(bctx *build.Context, pkg *build.Package, isTest bool, fileSet *token.FileSet) ([]*ast.File, error) {
 	var files []*ast.File
 	replacedDeclNames := make(map[string]bool)
-	funcName := func(d *ast.FuncDecl) string {
-		if d.Recv == nil || len(d.Recv.List) == 0 {
-			return d.Name.Name
-		}
-		recv := d.Recv.List[0].Type
-		if star, ok := recv.(*ast.StarExpr); ok {
-			recv = star.X
-		}
-		return recv.(*ast.Ident).Name + "." + d.Name.Name
-	}
+	pruneOriginalFuncs := make(map[string]bool)
+
 	isXTest := strings.HasSuffix(pkg.ImportPath, "_test")
 	importPath := pkg.ImportPath
 	if isXTest {
@@ -388,7 +380,9 @@ func parseAndAugment(bctx *build.Context, pkg *build.Package, isTest bool, fileS
 			for _, decl := range file.Decls {
 				switch d := decl.(type) {
 				case *ast.FuncDecl:
-					replacedDeclNames[funcName(d)] = true
+					k := astutil.FuncKey(d)
+					replacedDeclNames[k] = true
+					pruneOriginalFuncs[k] = astutil.PruneOriginal(d)
 				case *ast.GenDecl:
 					switch d.Tok {
 					case token.TYPE:
@@ -450,11 +444,14 @@ func parseAndAugment(bctx *build.Context, pkg *build.Package, isTest bool, fileS
 		for _, decl := range file.Decls {
 			switch d := decl.(type) {
 			case *ast.FuncDecl:
-				if replacedDeclNames[funcName(d)] {
+				k := astutil.FuncKey(d)
+				if replacedDeclNames[k] {
 					d.Name = ast.NewIdent("_")
-					// Prune function bodies, since it may contain code invalid for
-					// GopherJS and pin unwanted imports.
-					d.Body = nil
+					if pruneOriginalFuncs[k] {
+						// Prune function bodies, since it may contain code invalid for
+						// GopherJS and pin unwanted imports.
+						d.Body = nil
+					}
 				}
 			case *ast.GenDecl:
 				switch d.Tok {
@@ -474,20 +471,6 @@ func parseAndAugment(bctx *build.Context, pkg *build.Package, isTest bool, fileS
 							}
 						}
 					}
-				}
-			}
-		}
-
-		// Prune any imports that might have become unused after pruning function bodies.
-		for _, impGroup := range astutil.Imports(fileSet, file) {
-			for _, imp := range impGroup {
-				path, _ := strconv.Unquote(imp.Path.Value)
-				if !astutil.UsesImport(file, path) {
-					name := ""
-					if imp.Name != nil {
-						name = imp.Name.Name
-					}
-					astutil.DeleteNamedImport(fileSet, file, name, path)
 				}
 			}
 		}

compiler/astutil/astutil.go
@@ -3,6 +3,7 @@ package astutil
 import (
 	"go/ast"
 	"go/types"
+	"strings"
 )
 
 func RemoveParens(e ast.Expr) ast.Expr {
@@ -50,6 +51,40 @@ func IsTypeExpr(expr ast.Expr, info *types.Info) bool {
 func ImportsUnsafe(file *ast.File) bool {
 	for _, imp := range file.Imports {
 		if imp.Path.Value == `"unsafe"` {
+			return true
+		}
+	}
+	return false
+}
+
+// FuncKey returns a string, which uniquely identifies a top-level function or
+// method in a package.
+func FuncKey(d *ast.FuncDecl) string {
+	if d.Recv == nil || len(d.Recv.List) == 0 {
+		return d.Name.Name
+	}
+	recv := d.Recv.List[0].Type
+	if star, ok := recv.(*ast.StarExpr); ok {
+		recv = star.X
+	}
+	return recv.(*ast.Ident).Name + "." + d.Name.Name
+}
+
+// PruneOriginal returns true if gopherjs:prune-original directive is present
+// before a function decl.
+//
+// `//gopherjs:prune-original` is a GopherJS-specific directive, which can be
+// applied to functions in native overlays and will instruct the augmentation
+// logic to delete the body of a standard library function that was replaced.
+// This directive can be used to remove code that would be invalid in GopherJS,
+// such as code expecting ints to be 64-bit. It should be used with caution
+// since it may create unused imports in the original source file.
+func PruneOriginal(d *ast.FuncDecl) bool {
+	if d.Doc == nil {
+		return false
+	}
+	for _, c := range d.Doc.List {
+		if strings.HasPrefix(c.Text, "//gopherjs:prune-original") {
 			return true
 		}
 	}

compiler/astutil/astutil_test.go
@@ -1,6 +1,7 @@
 package astutil
 
 import (
+	"go/ast"
 	"go/parser"
 	"go/token"
 	"testing"
@@ -42,14 +43,112 @@ func TestImportsUnsafe(t *testing.T) {
 		t.Run(test.desc, func(t *testing.T) {
 			src := "package testpackage\n\n" + test.imports
 			fset := token.NewFileSet()
-			file, err := parser.ParseFile(fset, "test.go", src, parser.ParseComments)
-			if err != nil {
-				t.Fatalf("Failed to parse test source: %s", err)
-			}
+			file := parse(t, fset, src)
 			got := ImportsUnsafe(file)
 			if got != test.want {
 				t.Fatalf("ImportsUnsafe() returned %t, want %t", got, test.want)
 			}
 		})
 	}
+}
+
+func TestFuncKey(t *testing.T) {
+	tests := []struct {
+		desc string
+		src  string
+		want string
+	}{
+		{
+			desc: "top-level function",
+			src:  `package testpackage; func foo() {}`,
+			want: "foo",
+		}, {
+			desc: "top-level exported function",
+			src:  `package testpackage; func Foo() {}`,
+			want: "Foo",
+		}, {
+			desc: "method",
+			src:  `package testpackage; func (_ myType) bar() {}`,
+			want: "myType.bar",
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.desc, func(t *testing.T) {
+			fdecl := parseFuncDecl(t, test.src)
+			if got := FuncKey(fdecl); got != test.want {
+				t.Errorf("Got %q, want %q", got, test.want)
+			}
+		})
+	}
+}
+
+func TestPruneOriginal(t *testing.T) {
+	tests := []struct {
+		desc string
+		src  string
+		want bool
+	}{
+		{
+			desc: "no comment",
+			src: `package testpackage;
+			func foo() {}`,
+			want: false,
+		}, {
+			desc: "regular godoc",
+			src: `package testpackage;
+			// foo does something
+			func foo() {}`,
+			want: false,
+		}, {
+			desc: "only directive",
+			src: `package testpackage;
+			//gopherjs:prune-original
+			func foo() {}`,
+			want: true,
+		}, {
+			desc: "directive with explanation",
+			src: `package testpackage;
+			//gopherjs:prune-original because reasons
+			func foo() {}`,
+			want: true,
+		}, {
+			desc: "directive in godoc",
+			src: `package testpackage;
+			// foo does something
+			//gopherjs:prune-original
+			func foo() {}`,
+			want: true,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.desc, func(t *testing.T) {
+			fdecl := parseFuncDecl(t, test.src)
+			if got := PruneOriginal(fdecl); got != test.want {
+				t.Errorf("PruneOriginal() returned %t, want %t", got, test.want)
+			}
+		})
+	}
+}
+
+func parse(t *testing.T, fset *token.FileSet, src string) *ast.File {
+	t.Helper()
+	f, err := parser.ParseFile(fset, "test.go", src, parser.ParseComments)
+	if err != nil {
+		t.Fatalf("Failed to parse test source: %s", err)
+	}
+	return f
+}
+
+func parseFuncDecl(t *testing.T, src string) *ast.FuncDecl {
+	t.Helper()
+	fset := token.NewFileSet()
+	file := parse(t, fset, src)
+	if l := len(file.Decls); l != 1 {
+		t.Fatalf("Got %d decls in the sources, expected exactly 1", l)
+	}
+	fdecl, ok := file.Decls[0].(*ast.FuncDecl)
+	if !ok {
+		t.Fatalf("Got %T decl, expected *ast.FuncDecl", file.Decls[0])
+	}
+	return fdecl
 }

compiler/natives/src/syscall/js/export_test.go
@@ -1,0 +1,8 @@
+//+build js
+
+package js
+
+// Defined to avoid a compile error in the original TestGarbageCollection()
+// body. Can't use gopherjs:prune-original on it, since it causes an unused
+// import error.
+var JSGo Value

compiler/natives/src/syscall/js/js_test.go
@@ -4,6 +4,7 @@ package js_test
 
 import "testing"
 
+//gopherjs:prune-original
 func TestIntConversion(t *testing.T) {
 	// Same as upstream, but only test cases appropriate for a 32-bit environment.
 	testIntConversion(t, 0)
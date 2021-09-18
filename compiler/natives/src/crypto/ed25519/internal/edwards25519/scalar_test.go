c05f513e9256818755558202eb06e28373b1f5e4
Nevkontakte <aleks@nevkontakte.com>
2021-09-18 14:36:23 +0100

    Reduce the number of quickcheck iterations for edwards25519.
    This isn't really sufficient to test the implementation correctness
    perfectly, but it is a sufficient smoke test for GopherJS, and it cuts
    the test runtime substantially.

--- compiler/natives/src/crypto/ed25519/internal/edwards25519/scalar_test.go
+++ compiler/natives/src/crypto/ed25519/internal/edwards25519/scalar_test.go
@@ -3,20 +3,8 @@
 
 package edwards25519
 
-import "testing"
+import (
+	"testing/quick"
+)
 
-func TestScalarMultDistributesOverAdd(t *testing.T) {
-	t.Skip("slow") // Times out, takes ~13 minutes
-}
-
-func TestScalarMultNonIdentityPoint(t *testing.T) {
-	t.Skip("slow") // Takes > 5 min
-}
-
-func TestScalarMultMatchesBaseMult(t *testing.T) {
-	t.Skip("slow") // Takes > 5 min
-}
-
-func TestVarTimeDoubleBaseMultMatchesBaseMult(t *testing.T) {
-	t.Skip("slow") // Times out in CI
-}
+var quickCheckConfig32 = &quick.Config{MaxCount: 100}

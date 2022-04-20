46fb938bd778881edda319989f913aee48711209
Nevkontakte <aleks@nevkontakte.com>
2022-04-20 22:09:32 +0100

    Disable flaky net/http client tests.

---
+++ compiler/natives/src/net/http/client_test.go
@@ -0,0 +1,21 @@
+//go:build js
+
+package http_test
+
+import (
+	"testing"
+)
+
+func testClientTimeout(t *testing.T, h2 bool) {
+	// The original test expects Client.Timeout error to be returned, but under
+	// GopherJS an "i/o timeout" error is frequently returned. Otherwise the test
+	// seems to be working correctly.
+	t.Skip("Flaky test under GopherJS.")
+}
+
+func testClientTimeout_Headers(t *testing.T, h2 bool) {
+	// The original test expects Client.Timeout error to be returned, but under
+	// GopherJS an "i/o timeout" error is frequently returned. Otherwise the test
+	// seems to be working correctly.
+	t.Skip("Flaky test under GopherJS.")
+}

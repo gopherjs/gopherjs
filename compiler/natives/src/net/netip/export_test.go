7b2b95ab71cd78cd0be5bb35ac1abb4c52efbe5d
Nevkontakte <aleks@nevkontakte.com>
2022-06-04 19:25:41 +0100

    Make `internal/intern` unsupported and avoid its usage in `net/netip`.
    The optimization this package implements relies heavily on finalizers,
    which GopherJS doesn't support. As such it would create a memory leak
    rather than an actual optimization. Until we decide that we can use
    WeakMap API, this package can't be safely supported.
    
    The only current use of this package is in the `net/netip` package and
    this commit adds overlays that replace uses of the intern package with
    direct use of strings.

---
+++ compiler/natives/src/net/netip/export_test.go
@@ -0,0 +1,20 @@
+//go:build js
+
+package netip
+
+import (
+	"fmt"
+	"internal/intern"
+)
+
+//gopherjs:prune-original
+func MkAddr(u Uint128, z any) Addr {
+	switch z := z.(type) {
+	case *intern.Value:
+		return Addr{u, z.Get().(string)}
+	case string:
+		return Addr{u, z}
+	default:
+		panic(fmt.Errorf("unexpected type %T of the z argument"))
+	}
+}
---
+++ compiler/natives/src/net/netip/netip.go
@@ -0,0 +1,39 @@
+//go:build js
+
+package netip
+
+type Addr struct {
+	addr uint128
+	// Unlike the upstream, we store the string directly instead of trying to
+	// use internal/intern package for optimization.
+	z string
+}
+
+var (
+	// Sentinel values for different zones. \x00 character makes it unlikely for
+	// the sentinel value to clash with any real-life IPv6 zone index.
+	z0    = ""
+	z4    = "\x00ipv4"
+	z6noz = "\x00ipv6noz"
+)
+
+//gopherjs:prune-original
+func (ip Addr) Zone() string {
+	if ip.z == z4 || ip.z == z6noz {
+		return ""
+	}
+	return ip.z
+}
+
+//gopherjs:prune-original
+func (ip Addr) WithZone(zone string) Addr {
+	if !ip.Is6() {
+		return ip
+	}
+	if zone == "" {
+		ip.z = z6noz
+		return ip
+	}
+	ip.z = zone
+	return ip
+}

6d8aaecd34666e93a968756ce0c95c4317f4b37f
Nevkontakte <aleks@nevkontakte.com>
2022-07-16 20:47:51 +0100

    Allow multiple "_" fields in `reflect.StructOf()`.
    This addresses https://github.com/golang/go/issues/49110 for GopherJS.

--- compiler/natives/src/reflect/reflect.go
+++ compiler/natives/src/reflect/reflect.go
@@ -495,7 +495,7 @@ func StructOf(fields []StructField) Type {
 			}
 		}
 
-		if _, dup := fset[name]; dup {
+		if _, dup := fset[name]; dup && name != "_" {
 			panic("reflect.StructOf: duplicate field " + name)
 		}
 		fset[name] = struct{}{}

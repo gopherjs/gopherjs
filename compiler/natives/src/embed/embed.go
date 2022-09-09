61801edac205a0a23513d3758713cefb05da084b
visualfc <visualfc@gmail.com>
2022-09-09 21:38:04 +0800

    compiler/natives/src: add embed

---
+++ compiler/natives/src/embed/embed.go
@@ -0,0 +1,21 @@
+//go:build js
+// +build js
+
+package embed
+
+func buildFS(list []struct {
+	name string
+	data string
+	hash [16]byte
+}) (f FS) {
+	var files []file
+	for _, v := range list {
+		files = append(files, file{
+			name: v.name,
+			data: v.data,
+			hash: v.hash,
+		})
+	}
+	f.files = &files
+	return
+}

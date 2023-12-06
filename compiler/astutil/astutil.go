892b5e228c80969e82c97e4014cd14b27af5734c
Grant Nelson <grant.nelson@workiva.com>
2023-12-06 14:39:17 -0700

    Added directives

--- compiler/astutil/astutil.go
+++ compiler/astutil/astutil.go
@@ -5,6 +5,7 @@ import (
 	"go/ast"
 	"go/token"
 	"go/types"
+	"regexp"
 )
 
 func RemoveParens(e ast.Expr) ast.Expr {
@@ -69,6 +70,103 @@ func FuncKey(d *ast.FuncDecl) string {
 		recv = star.X
 	}
 	return recv.(*ast.Ident).Name + "." + d.Name.Name
+}
+
+// anyDocLine calls the given predicate on all associated documentation
+// lines and line-comment lines from the given node.
+// If the predicate returns true for any line then true is returned.
+func anyDocLine(node any, predicate func(line string) bool) bool {
+	switch a := node.(type) {
+	case *ast.Comment:
+		return a != nil && predicate(a.Text)
+	case *ast.CommentGroup:
+		if a != nil {
+			for _, c := range a.List {
+				if anyDocLine(c, predicate) {
+					return true
+				}
+			}
+		}
+		return false
+	case *ast.Field:
+		return a != nil && (anyDocLine(a.Doc, predicate) || anyDocLine(a.Comment, predicate))
+	case *ast.File:
+		return a != nil && anyDocLine(a.Doc, predicate)
+	case *ast.FuncDecl:
+		return a != nil && anyDocLine(a.Doc, predicate)
+	case *ast.GenDecl:
+		return a != nil && anyDocLine(a.Doc, predicate)
+	case *ast.ImportSpec:
+		return a != nil && (anyDocLine(a.Doc, predicate) || anyDocLine(a.Comment, predicate))
+	case *ast.TypeSpec:
+		return a != nil && (anyDocLine(a.Doc, predicate) || anyDocLine(a.Comment, predicate))
+	case *ast.ValueSpec:
+		return a != nil && (anyDocLine(a.Doc, predicate) || anyDocLine(a.Comment, predicate))
+	default:
+		panic(fmt.Errorf(`unexpected node type to get doc from: %T`, node))
+	}
+}
+
+// directiveMatcher is a regex which matches a GopherJS directive
+// and finds the directive action.
+//
+// This matches the largest directive action until whitespace or EOL
+// to differentiate from any directive action which is a prefix
+// for another directive action.
+var directiveMatcher = regexp.MustCompile(`^\/(?:\/|\*)gopherjs:([\w-]+)`)
+
+// hasDirective returns true if the associated documentation
+// or line comments for the given node have the given directive action.
+//
+// All gopherjs directives must start with `//gopherjs:` or `/*gopherjs:`
+// and followed by an action without any whitespace. The action must be
+// one or more letter, decimal, underscore, or hyphen.
+//
+// see https://pkg.go.dev/cmd/compile#hdr-Compiler_Directives
+func hasDirective(node any, directive string) bool {
+	return anyDocLine(node, func(line string) bool {
+		m := directiveMatcher.FindStringSubmatch(line)
+		return len(m) == 2 && m[1] == directive
+	})
+}
+
+// PruneOriginal returns true if gopherjs:prune-original directive is present
+// before a function decl.
+//
+// `//gopherjs:prune-original` is a GopherJS-specific directive, which can be
+// applied to functions in native overlays and will instruct the augmentation
+// logic to delete the body of a standard library function that was replaced.
+// This directive can be used to remove code that would be invalid in GopherJS,
+// such as code expecting ints to be 64-bit. It should be used with caution
+// since it may create unused imports in the original source file.
+func PruneOriginal(d *ast.FuncDecl) bool {
+	return hasDirective(d, `prune-original`)
+}
+
+// KeepOriginal returns true if gopherjs:keep-original directive is present
+// before a function decl.
+//
+// `//gopherjs:keep-original` is a GopherJS-specific directive, which can be
+// applied to functions in native overlays and will instruct the augmentation
+// logic to expose the original function such that it can be called. For a
+// function in the original called `foo`, it will be accessible by the name
+// `_gopherjs_original_foo`.
+func KeepOriginal(d *ast.FuncDecl) bool {
+	return hasDirective(d, `keep-original`)
+}
+
+// Purge returns true if gopherjs:purge directive is present
+// on a struct, interface, variables, constants, and functions.
+//
+// `//gopherjs:purge` is a GopherJS-specific directive, which can be
+// applied in native overlays and will instruct the augmentation logic to
+// delete part of the standard library without a replacement. This directive
+// can be used to remove code that would be invalid in GopherJS, such as code
+// using unsupported features (e.g. generic interfaces and methods before
+// generics were fully supported). It should be used with caution since it
+// may remove needed dependencies.
+func Purge(d any) bool {
+	return hasDirective(d, `purge`)
 }
 
 // FindLoopStmt tries to find the loop statement among the AST nodes in the
--- compiler/astutil/astutil_test.go
+++ compiler/astutil/astutil_test.go
@@ -1,6 +1,8 @@
 package astutil
 
 import (
+	"fmt"
+	"go/ast"
 	"go/token"
 	"testing"
 
@@ -77,6 +79,416 @@ func TestFuncKey(t *testing.T) {
 			fdecl := srctesting.ParseFuncDecl(t, test.src)
 			if got := FuncKey(fdecl); got != test.want {
 				t.Errorf("Got %q, want %q", got, test.want)
+			}
+		})
+	}
+}
+
+func TestPruneOriginal(t *testing.T) {
+	tests := []struct {
+		desc string
+		src  string
+		want bool
+	}{
+		{
+			desc: "no comment",
+			src: `package testpackage;
+			func foo() {}`,
+			want: false,
+		}, {
+			desc: "regular godoc",
+			src: `package testpackage;
+			// foo does something
+			func foo() {}`,
+			want: false,
+		}, {
+			desc: "only directive",
+			src: `package testpackage;
+			//gopherjs:prune-original
+			func foo() {}`,
+			want: true,
+		}, {
+			desc: "directive with explanation",
+			src: `package testpackage;
+			//gopherjs:prune-original because reasons
+			func foo() {}`,
+			want: true,
+		}, {
+			desc: "directive in godoc",
+			src: `package testpackage;
+			// foo does something
+			//gopherjs:prune-original
+			func foo() {}`,
+			want: true,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.desc, func(t *testing.T) {
+			fdecl := srctesting.ParseFuncDecl(t, test.src)
+			if got := PruneOriginal(fdecl); got != test.want {
+				t.Errorf("PruneOriginal() returned %t, want %t", got, test.want)
+			}
+		})
+	}
+}
+
+func TestHasDirectiveOnDecl(t *testing.T) {
+	tests := []struct {
+		desc string
+		src  string
+		want bool
+	}{
+		{
+			desc: `no comment on function`,
+			src: `package testpackage;
+				func foo() {}`,
+			want: false,
+		}, {
+			desc: `no directive on function with comment`,
+			src: `package testpackage;
+				// foo has no directive
+				func foo() {}`,
+			want: false,
+		}, {
+			desc: `wrong directive on function`,
+			src: `package testpackage;
+				//gopherjs:wrong-directive
+				func foo() {}`,
+			want: false,
+		}, {
+			desc: `correct directive on function`,
+			src: `package testpackage;
+				//gopherjs:do-stuff
+				// foo has a directive to do stuff
+				func foo() {}`,
+			want: true,
+		}, {
+			desc: `correct directive in multiline comment on function`,
+			src: `package testpackage;
+				/*gopherjs:do-stuff
+				  foo has a directive to do stuff
+				*/
+				func foo() {}`,
+			want: true,
+		}, {
+			desc: `invalid directive in multiline comment on function`,
+			src: `package testpackage;
+				/*
+				gopherjs:do-stuff
+				*/
+				func foo() {}`,
+			want: false,
+		}, {
+			desc: `prefix directive on function`,
+			src: `package testpackage;
+				//gopherjs:do-stuffs
+				func foo() {}`,
+			want: false,
+		}, {
+			desc: `multiple directives on function`,
+			src: `package testpackage;
+				//gopherjs:wrong-directive
+				//gopherjs:do-stuff
+				//gopherjs:another-directive
+				func foo() {}`,
+			want: true,
+		}, {
+			desc: `directive with explanation on function`,
+			src: `package testpackage;
+				//gopherjs:do-stuff 'cause we can
+				func foo() {}`,
+			want: true,
+		}, {
+			desc: `no directive on type declaration`,
+			src: `package testpackage;
+				// Foo has a comment
+				type Foo int`,
+			want: false,
+		}, {
+			desc: `directive on type declaration`,
+			src: `package testpackage;
+				//gopherjs:do-stuff
+				type Foo int`,
+			want: true,
+		}, {
+			desc: `no directive on const declaration`,
+			src: `package testpackage;
+				const foo = 42`,
+			want: false,
+		}, {
+			desc: `directive on const documentation`,
+			src: `package testpackage;
+				//gopherjs:do-stuff
+				const foo = 42`,
+			want: true,
+		}, {
+			desc: `no directive on var declaration`,
+			src: `package testpackage;
+				var foo = 42`,
+			want: false,
+		}, {
+			desc: `directive on var documentation`,
+			src: `package testpackage;
+				//gopherjs:do-stuff
+				var foo = 42`,
+			want: true,
+		}, {
+			desc: `no directive on var declaration`,
+			src: `package testpackage;
+				import _ "embed"`,
+			want: false,
+		}, {
+			desc: `directive on var documentation`,
+			src: `package testpackage;
+				//gopherjs:do-stuff
+				import _ "embed"`,
+			want: true,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.desc, func(t *testing.T) {
+			const action = `do-stuff`
+			decl := srctesting.ParseDecl(t, test.src)
+			if got := hasDirective(decl, action); got != test.want {
+				t.Errorf(`hasDirective(%T, %q) returned %t, want %t`, decl, action, got, test.want)
+			}
+		})
+	}
+}
+
+func TestHasDirectiveOnSpec(t *testing.T) {
+	tests := []struct {
+		desc string
+		src  string
+		want bool
+	}{
+		{
+			desc: `no directive on type specification`,
+			src: `package testpackage;
+				type Foo int`,
+			want: false,
+		}, {
+			desc: `directive in doc on type specification`,
+			src: `package testpackage;
+				type (
+					//gopherjs:do-stuff
+					Foo int
+				)`,
+			want: true,
+		}, {
+			desc: `directive in line on type specification`,
+			src: `package testpackage;
+				type Foo int //gopherjs:do-stuff`,
+			want: true,
+		}, {
+			desc: `no directive on const specification`,
+			src: `package testpackage;
+				const foo = 42`,
+			want: false,
+		}, {
+			desc: `directive in doc on const specification`,
+			src: `package testpackage;
+				const (
+					//gopherjs:do-stuff
+					foo = 42
+				)`,
+			want: true,
+		}, {
+			desc: `directive in line on const specification`,
+			src: `package testpackage;
+				const foo = 42 //gopherjs:do-stuff`,
+			want: true,
+		}, {
+			desc: `no directive on var specification`,
+			src: `package testpackage;
+				var foo = 42`,
+			want: false,
+		}, {
+			desc: `directive in doc on var specification`,
+			src: `package testpackage;
+				var (
+					//gopherjs:do-stuff
+					foo = 42
+				)`,
+			want: true,
+		}, {
+			desc: `directive in line on var specification`,
+			src: `package testpackage;
+				var foo = 42 //gopherjs:do-stuff`,
+			want: true,
+		}, {
+			desc: `no directive on import specification`,
+			src: `package testpackage;
+				import _ "embed"`,
+			want: false,
+		}, {
+			desc: `directive in doc on import specification`,
+			src: `package testpackage;
+				import (
+					//gopherjs:do-stuff
+					_ "embed"
+				)`,
+			want: true,
+		}, {
+			desc: `directive in line on import specification`,
+			src: `package testpackage;
+				import _ "embed" //gopherjs:do-stuff`,
+			want: true,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.desc, func(t *testing.T) {
+			const action = `do-stuff`
+			spec := srctesting.ParseSpec(t, test.src)
+			if got := hasDirective(spec, action); got != test.want {
+				t.Errorf(`hasDirective(%T, %q) returned %t, want %t`, spec, action, got, test.want)
+			}
+		})
+	}
+}
+
+func TestHasDirectiveOnFile(t *testing.T) {
+	tests := []struct {
+		desc string
+		src  string
+		want bool
+	}{
+		{
+			desc: `no directive on file`,
+			src: `package testpackage;
+				//gopherjs:do-stuff
+				type Foo int`,
+			want: false,
+		}, {
+			desc: `directive on file`,
+			src: `//gopherjs:do-stuff
+				package testpackage;
+				type Foo int`,
+			want: true,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.desc, func(t *testing.T) {
+			const action = `do-stuff`
+			fset := token.NewFileSet()
+			file := srctesting.Parse(t, fset, test.src)
+			if got := hasDirective(file, action); got != test.want {
+				t.Errorf(`hasDirective(%T, %q) returned %t, want %t`, file, action, got, test.want)
+			}
+		})
+	}
+}
+
+func TestHasDirectiveOnField(t *testing.T) {
+	tests := []struct {
+		desc string
+		src  string
+		want bool
+	}{
+		{
+			desc: `no directive on struct field`,
+			src: `package testpackage;
+				type Foo struct {
+					bar int
+				}`,
+			want: false,
+		}, {
+			desc: `directive in doc on struct field`,
+			src: `package testpackage;
+				type Foo struct {
+					//gopherjs:do-stuff
+					bar int
+				}`,
+			want: true,
+		}, {
+			desc: `directive in line on struct field`,
+			src: `package testpackage;
+				type Foo struct {
+					bar int //gopherjs:do-stuff
+				}`,
+			want: true,
+		}, {
+			desc: `no directive on interface method`,
+			src: `package testpackage;
+				type Foo interface {
+					Bar(a int) int
+				}`,
+			want: false,
+		}, {
+			desc: `directive in doc on interface method`,
+			src: `package testpackage;
+				type Foo interface {
+					//gopherjs:do-stuff
+					Bar(a int) int
+				}`,
+			want: true,
+		}, {
+			desc: `directive in line on interface method`,
+			src: `package testpackage;
+				type Foo interface {
+					Bar(a int) int //gopherjs:do-stuff
+				}`,
+			want: true,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.desc, func(t *testing.T) {
+			const action = `do-stuff`
+			spec := srctesting.ParseSpec(t, test.src)
+			tspec := spec.(*ast.TypeSpec)
+			var field *ast.Field
+			switch typeNode := tspec.Type.(type) {
+			case *ast.StructType:
+				field = typeNode.Fields.List[0]
+			case *ast.InterfaceType:
+				field = typeNode.Methods.List[0]
+			default:
+				t.Errorf(`unexpected node type, %T, when finding field`, typeNode)
+				return
+			}
+			if got := hasDirective(field, action); got != test.want {
+				t.Errorf(`hasDirective(%T, %q) returned %t, want %t`, field, action, got, test.want)
+			}
+		})
+	}
+}
+
+func TestHasDirectiveBadCase(t *testing.T) {
+	tests := []struct {
+		desc string
+		node any
+		want string
+	}{
+		{
+			desc: `untyped nil node`,
+			node: nil,
+			want: `unexpected node type to get doc from: <nil>`,
+		}, {
+			desc: `unexpected node type`,
+			node: &ast.ArrayType{},
+			want: `unexpected node type to get doc from: *ast.ArrayType`,
+		}, {
+			desc: `nil expected node type`,
+			node: (*ast.FuncDecl)(nil),
+			want: `<nil>`, // no panic
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.desc, func(t *testing.T) {
+			const action = `do-stuff`
+
+			var got string
+			func() {
+				defer func() { got = fmt.Sprint(recover()) }()
+				hasDirective(test.node, action)
+			}()
+			if got != test.want {
+				t.Errorf(`hasDirective(%T, %q) returned %s, want %s`, test.node, action, got, test.want)
 			}
 		})
 	}

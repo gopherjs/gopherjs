f6a061b484e4b7e4887aa5d512464f9f43d80630
Grant Nelson <grant.nelson@workiva.com>
2024-01-10 09:47:11 -0700

    Working on crypto

--- compiler/natives/src/crypto/elliptic/nistec.go
+++ compiler/natives/src/crypto/elliptic/nistec.go
@@ -6,6 +6,7 @@ package elliptic
 import (
 	"crypto/internal/nistec"
 	"reflect"
+	"sync"
 )
 
 // The following code changes in this file are to make p224, p256,
@@ -14,17 +15,46 @@ import (
 //gopherjs:purge for go1.19 without generics
 type nistPoint[T any] interface{}
 
+type reflectivePointCaller struct {
+	Bytes          func(recv any) []byte
+	SetBytes       func(recv any, b []byte) (any, error)
+	Add            func(recv, p1, p2 any) any
+	Double         func(recv, p1 any) any
+	ScalarMult     func(recv, p1 any, scalar []byte) (any, error)
+	ScalarBaseMult func(recv any, scalar []byte) (any, error)
+}
+
+var pointCallerInit sync.Once
+var pointCallerSingleton *reflectivePointCaller
+
+func initPointCallerInstance() {
+	pointCallerInit.Do(func() {
+		wrapFunc := func(fName string, fPtr any) {
+			fn := reflect.ValueOf(fPtr).Elem()
+			caller := func(args []reflect.Value) []reflect.Value {
+				return args[0].MethodByName(fName).Call(args[1:])
+			}
+			v := reflect.MakeFunc(fn.Type(), caller)
+			fn.Set(v)
+		}
+
+		rpc := &reflectivePointCaller{}
+		wrapFunc(`Bytes`, &rpc.Bytes)
+		wrapFunc(`SetBytes`, &rpc.SetBytes)
+		wrapFunc(`Add`, &rpc.Add)
+		wrapFunc(`Double`, &rpc.Double)
+		wrapFunc(`ScalarMult`, &rpc.ScalarMult)
+		wrapFunc(`ScalarBaseMult`, &rpc.ScalarBaseMult)
+		pointCallerSingleton = rpc
+	})
+}
+
 type wrappedPoint struct {
-	point              any
-	funcBytes          func() []byte
-	funcSetBytes       func(b []byte) (any, error)
-	funcAdd            func(p1, p2 any) any
-	funcDouble         func(p1 any) any
-	funcScalarMult     func(p1 any, scalar []byte) (any, error)
-	funcScalarBaseMult func(scalar []byte) (any, error)
+	point any
 }
 
 func newWrappedPoint(point any) *wrappedPoint {
+	initPointCallerInstance()
 	return &wrappedPoint{point: point}
 }
 
@@ -34,55 +64,30 @@ func newPointWrapper(newPoint func() any) func() *wrappedPoint {
 	}
 }
 
-func (w *wrappedPoint) wrapFunc(fName string, fPtr any) {
-	fn := reflect.ValueOf(fPtr).Elem()
-	target := reflect.ValueOf(w.point).MethodByName(fName).Call
-	v := reflect.MakeFunc(fn.Type(), target)
-	fn.Set(v)
-}
-
 func (w *wrappedPoint) Bytes() []byte {
-	if w.funcBytes == nil {
-		w.wrapFunc(`Bytes`, &w.funcBytes)
-	}
-	return w.funcBytes()
+	return pointCallerSingleton.Bytes(w.point)
 }
 
 func (w *wrappedPoint) SetBytes(b []byte) (*wrappedPoint, error) {
-	if w.funcSetBytes == nil {
-		w.wrapFunc(`SetBytes`, &w.funcSetBytes)
-	}
-	p, err := w.funcSetBytes(b)
+	p, err := pointCallerSingleton.SetBytes(w.point, b)
 	return newWrappedPoint(p), err
 }
 
 func (w *wrappedPoint) Add(w1, w2 *wrappedPoint) *wrappedPoint {
-	if w.funcAdd == nil {
-		w.wrapFunc(`Add`, &w.funcAdd)
-	}
-	return newWrappedPoint(w.funcAdd(w1.point, w2.point))
+	return newWrappedPoint(pointCallerSingleton.Add(w.point, w1.point, w2.point))
 }
 
 func (w *wrappedPoint) Double(w1 *wrappedPoint) *wrappedPoint {
-	if w.funcDouble == nil {
-		w.wrapFunc(`Double`, &w.funcDouble)
-	}
-	return newWrappedPoint(w.funcDouble(w1.point))
+	return newWrappedPoint(pointCallerSingleton.Double(w.point, w1.point))
 }
 
 func (w *wrappedPoint) ScalarMult(w1 *wrappedPoint, scalar []byte) (*wrappedPoint, error) {
-	if w.funcScalarMult == nil {
-		w.wrapFunc(`ScalarMult`, &w.funcScalarMult)
-	}
-	p, err := w.funcScalarMult(w1.point, scalar)
+	p, err := pointCallerSingleton.ScalarMult(w.point, w1.point, scalar)
 	return newWrappedPoint(p), err
 }
 
 func (w *wrappedPoint) ScalarBaseMult(scalar []byte) (*wrappedPoint, error) {
-	if w.funcScalarBaseMult == nil {
-		w.wrapFunc(`ScalarBaseMult`, &w.funcScalarBaseMult)
-	}
-	p, err := w.funcScalarBaseMult(scalar)
+	p, err := pointCallerSingleton.ScalarBaseMult(w.point, scalar)
 	return newWrappedPoint(p), err
 }
 

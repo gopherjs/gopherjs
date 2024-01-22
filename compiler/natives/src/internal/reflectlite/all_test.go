581487775c8ebb463c3430374c57521c9969590b
Grant Nelson <grant.nelson@workiva.com>
2024-01-22 10:58:18 -0700

    Updating reflectlite name test

--- compiler/natives/src/internal/reflectlite/all_test.go
+++ compiler/natives/src/internal/reflectlite/all_test.go
@@ -21,3 +21,27 @@ func TestTypes(t *testing.T) {
 func TestNameBytesAreAligned(t *testing.T) {
 	t.Skip("TestNameBytesAreAligned")
 }
+
+// `A` is used with `B[T any]` and is otherwise not needed.
+//
+//gopherjs:purge for go1.19 without generics
+type A struct{}
+
+//gopherjs:purge for go1.19 without generics
+type B[T any] struct{}
+
+// removing the name tests using `B[T any]` for go1.19 without generics
+var nameTests = []nameTest{
+	{(*int32)(nil), "int32"},
+	{(*D1)(nil), "D1"},
+	{(*[]D1)(nil), ""},
+	{(*chan D1)(nil), ""},
+	{(*func() D1)(nil), ""},
+	{(*<-chan D1)(nil), ""},
+	{(*chan<- D1)(nil), ""},
+	{(*any)(nil), ""},
+	{(*interface {
+		F()
+	})(nil), ""},
+	{(*TheNameOfThisTypeIsExactly255BytesLongSoWhenTheCompilerPrependsTheReflectTestPackageNameAndExtraStarTheLinkerRuntimeAndReflectPackagesWillHaveToCorrectlyDecodeTheSecondLengthByte0123456789_0123456789_0123456789_0123456789_0123456789_012345678)(nil), "TheNameOfThisTypeIsExactly255BytesLongSoWhenTheCompilerPrependsTheReflectTestPackageNameAndExtraStarTheLinkerRuntimeAndReflectPackagesWillHaveToCorrectlyDecodeTheSecondLengthByte0123456789_0123456789_0123456789_0123456789_0123456789_012345678"},
+}

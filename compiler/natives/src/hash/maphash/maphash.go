77335deab89f49233fb671c701f90c1a913096a9
Grant Nelson <grant.nelson@workiva.com>
2024-01-22 12:34:48 -0700

    Updating maphash

--- compiler/natives/src/hash/maphash/maphash.go
+++ compiler/natives/src/hash/maphash/maphash.go
@@ -8,7 +8,7 @@ var hashkey [4]uint32
 
 func init() {
 	for i := range hashkey {
-		hashkey[i] = uint32(runtime_fastrand64())
+		hashkey[i] = runtime_fastrand()
 	}
 	hashkey[0] |= 1 // make sure these numbers are odd
 	hashkey[1] |= 1
@@ -16,17 +16,76 @@ func init() {
 	hashkey[3] |= 1
 }
 
-func _rthash(b []byte, seed uint64) uint64 {
+//go:linkname runtime_fastrand runtime.fastrand
+func runtime_fastrand() uint32
+
+// Bytes is overwritten to avoid more efficient equivalent
+// using unsafe via original rthash.
+func Bytes(seed Seed, b []byte) uint64 {
+	var h Hash
+	h.SetSeed(seed)
+	_, _ = h.Write(b)
+	return h.Sum64()
+}
+
+// String is overwritten to avoid more efficient equivalent
+// using unsafe via original rthash.
+func String(seed Seed, s string) uint64 {
+	var h Hash
+	h.SetSeed(seed)
+	_, _ = h.WriteString(s)
+	return h.Sum64()
+}
+
+// Write and WriteString are a simplification from Go 1.19.13
+// with the call to rthash changed to not use unsafe pointers.
+// See https://cs.opensource.google/go/go/+/refs/tags/go1.19.13:src/hash/maphash/maphash.go;l=146
+func (h *Hash) Write(b []byte) (int, error) {
+	size := len(b)
+	if h.n+len(b) > bufSize {
+		h.initSeed()
+		for h.n+len(b) > bufSize {
+			k := copy(h.buf[h.n:], b)
+			h.state.s = rthash(h.buf[:bufSize], h.state.s)
+			b = b[k:]
+			h.n = 0
+		}
+	}
+	h.n += copy(h.buf[h.n:], b)
+	return size, nil
+}
+
+func (h *Hash) WriteString(s string) (int, error) {
+	size := len(s)
+	if h.n+len(s) > bufSize {
+		h.initSeed()
+		for h.n+len(s) > bufSize {
+			k := copy(h.buf[h.n:], s)
+			h.state.s = rthash(h.buf[:bufSize], h.state.s)
+			s = s[k:]
+			h.n = 0
+		}
+	}
+	h.n += copy(h.buf[h.n:], s)
+	return size, nil
+}
+
+// rthash is similar to the Go 1.19.13 version
+// with the call to memhash changed to not use unsafe pointers.
+func rthash(b []byte, seed uint64) uint64 {
 	if len(b) == 0 {
 		return seed
 	}
 	// The runtime hasher only works on uintptr. Since GopherJS implements a
 	// 32-bit environment, we use two parallel hashers on the lower and upper 32
 	// bits.
-	lo := memhash(b, uint32(seed), uint32(len(b)))
-	hi := memhash(b, uint32(seed>>32), uint32(len(b)))
+	lo := memhash(b, uint32(seed))
+	hi := memhash(b, uint32(seed>>32))
 	return uint64(hi)<<32 | uint64(lo)
 }
+
+//gopherjs:purge to remove link using unsafe pointers, use memhash instead.
+func runtime_memhash()
 
 // The implementation below is adapted from the upstream runtime/hash32.go
 // and avoids use of unsafe, which GopherJS doesn't support well and leads to
@@ -38,8 +97,9 @@ func _rthash(b []byte, seed uint64) uint64 {
 //
 // Hashing algorithm inspired by wyhash:
 // https://github.com/wangyi-fudan/wyhash/blob/ceb019b530e2c1c14d70b79bfa2bc49de7d95bc1/Modern%20Non-Cryptographic%20Hash%20Function%20and%20Pseudorandom%20Number%20Generator.pdf
-func memhash(p []byte, seed uint32, s uint32) uintptr {
-	a, b := mix32(uint32(seed), uint32(s^hashkey[0]))
+func memhash(p []byte, seed uint32) uintptr {
+	s := len(p)
+	a, b := mix32(uint32(seed), uint32(s)^hashkey[0])
 	if s == 0 {
 		return uintptr(a ^ b)
 	}
@@ -63,7 +123,7 @@ func memhash(p []byte, seed uint32, s uint32) uintptr {
 	return uintptr(a ^ b)
 }
 
-func add(p []byte, x uint32) []byte {
+func add(p []byte, x int) []byte {
 	return p[x:]
 }
 
@@ -77,54 +137,20 @@ func mix32(a, b uint32) (uint32, uint32) {
 	return uint32(c), uint32(c >> 32)
 }
 
-/*
-	The following functions were modified in Go 1.17 to improve performance,
-	but at the expense of being unsafe, and thus incompatible with GopherJS.
-	To compensate, we have reverted these to the unoptimized Go 1.16 versions
-	for now.
-
-	See upstream issue https://github.com/golang/go/issues/47342 to implement
-	a purego version of this package, which should render this hack (and
-	likely this entire file) obsolete.
-*/
-
-// Write is borrowed from Go 1.16.
-func (h *Hash) Write(b []byte) (int, error) {
-	size := len(b)
-	for h.n+len(b) > len(h.buf) {
-		k := copy(h.buf[h.n:], b)
-		h.n = len(h.buf)
-		b = b[k:]
-		h.flush()
-	}
-	h.n += copy(h.buf[h.n:], b)
-	return size, nil
-}
-
-// WriteString is borrowed from Go 1.16.
-func (h *Hash) WriteString(s string) (int, error) {
-	size := len(s)
-	for h.n+len(s) > len(h.buf) {
-		k := copy(h.buf[h.n:], s)
-		h.n = len(h.buf)
-		s = s[k:]
-		h.flush()
-	}
-	h.n += copy(h.buf[h.n:], s)
-	return size, nil
-}
-
+// flush is the Go 1.19.13 version
+// with the call to rthash changed to not use unsafe pointers.
 func (h *Hash) flush() {
 	if h.n != len(h.buf) {
 		panic("maphash: flush of partially full buffer")
 	}
 	h.initSeed()
-	h.state.s = _rthash(h.buf[:], h.state.s)
+	h.state.s = rthash(h.buf, h.state.s)
 	h.n = 0
 }
 
-// Sum64 is borrowed from Go 1.16.
+// Sum64 is the Go 1.19.13 version
+// with the call to rthash changed to not use unsafe pointers.
 func (h *Hash) Sum64() uint64 {
 	h.initSeed()
-	return _rthash(h.buf[:h.n], h.state.s)
+	return rthash(h.buf[:h.n], h.state.s)
 }

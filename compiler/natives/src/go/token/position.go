1ca4b3355128777165330efcc2b399c67fd14c06
Grant Nelson <grant.nelson@workiva.com>
2024-01-24 09:05:31 -0700

    Trying to fix TestLineComments

--- compiler/natives/src/go/token/position.go
+++ compiler/natives/src/go/token/position.go
@@ -5,8 +5,6 @@ package token
 
 import (
 	"sync"
-	"sync/atomic"
-	"unsafe"
 )
 
 type FileSet struct {
@@ -14,13 +12,13 @@ type FileSet struct {
 	base  int
 	files []*File
 
-	// replaced atomic.Pointer[File] for go1.19 without generics
+	// replaced atomic.Pointer[File] for go1.19 without generics.
 	last atomicFilePointer
 }
 
 type atomicFilePointer struct {
-	v unsafe.Pointer
+	v *File
 }
 
-func (x *atomicFilePointer) Load() *File     { return (*File)(atomic.LoadPointer(&x.v)) }
-func (x *atomicFilePointer) Store(val *File) { atomic.StorePointer(&x.v, unsafe.Pointer(val)) }
+func (x *atomicFilePointer) Load() *File     { return x.v }
+func (x *atomicFilePointer) Store(val *File) { x.v = val }

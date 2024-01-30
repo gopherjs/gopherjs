919d04bb9b7a1d258a687694afe7003aa39a6a75
Grant Nelson <grant.nelson@workiva.com>
2024-01-29 19:52:33 -0700

    Fixing readCOFFSymbolAuxInfo

--- compiler/natives/src/debug/pe/symbol.go
+++ compiler/natives/src/debug/pe/symbol.go
@@ -27,23 +27,28 @@ func (buf *bytesBufferLite) Read(p []byte) (int, error) {
 	return n, nil
 }
 
-func copyToAuxFormat5(sym *COFFSymbol) *COFFSymbolAuxFormat5 {
-	order := binary.LittleEndian
-	buf := &bytesBufferLite{data: make([]byte, 0, 16)}
-	_ = binary.Write(buf, order, sym.Name[:])
-	_ = binary.Write(buf, order, sym.Value)
-	_ = binary.Write(buf, order, sym.SectionNumber)
-	_ = binary.Write(buf, order, sym.Type)
-	// skip StorageClass and NumberOfAuxSymbols
-
+func copyToAuxFormat5(sym *COFFSymbol) (*COFFSymbolAuxFormat5, error) {
+	buf := &bytesBufferLite{data: make([]byte, 0, 20)}
+	if err := binary.Write(buf, binary.LittleEndian, sym); err != nil {
+		return nil, err
+	}
 	aux := &COFFSymbolAuxFormat5{}
-	_ = binary.Read(buf, order, &aux.Size)
-	_ = binary.Read(buf, order, &aux.NumRelocs)
-	_ = binary.Read(buf, order, &aux.NumLineNumbers)
-	_ = binary.Read(buf, order, &aux.Checksum)
-	_ = binary.Read(buf, order, &aux.SecNum)
-	_ = binary.Read(buf, order, &aux.Selection)
-	return aux
+	if err := binary.Read(buf, binary.LittleEndian, aux); err != nil {
+		return nil, err
+	}
+	return aux, nil
+}
+
+func copyFromAuxFormat5(aux *COFFSymbolAuxFormat5) (*COFFSymbol, error) {
+	buf := &bytesBufferLite{data: make([]byte, 0, 20)}
+	if err := binary.Write(buf, binary.LittleEndian, aux); err != nil {
+		return nil, err
+	}
+	sym := &COFFSymbol{}
+	if err := binary.Read(buf, binary.LittleEndian, sym); err != nil {
+		return nil, err
+	}
+	return sym, nil
 }
 
 func readCOFFSymbols(fh *FileHeader, r io.ReadSeeker) ([]COFFSymbol, error) {
@@ -68,16 +73,21 @@ func readCOFFSymbols(fh *FileHeader, r io.ReadSeeker) ([]COFFSymbol, error) {
 			naux = int(syms[k].NumberOfAuxSymbols)
 		} else {
 			naux--
-			// The following was reading one struct as another struct with
-			// the same memory footprint. This doesn't work in JS so the
-			// `rv` value is left with a bunch of `undefined`s. So replace
-			// raux := (*COFFSymbolAuxFormat5)(unsafe.Pointer(&syms[k]))
-			// (an in memory remap) with the following copy.
-			aux := copyToAuxFormat5(&syms[k])
+			// The following was reading into one struct with the same memory
+			// footprint as another struck. This doesn't work in JS so the
+			// `syms` value is left with a bunch of defaults. So replace
+			// aux := (*COFFSymbolAuxFormat5)(unsafe.Pointer(&syms[k]))
+			// (an in memory remap) with the following read and then copy.
+			aux := &COFFSymbolAuxFormat5{}
 			err = binary.Read(r, binary.LittleEndian, aux)
 			if err != nil {
 				return nil, fmt.Errorf("fail to read symbol table: %v", err)
 			}
+			pesymn, err := copyFromAuxFormat5(aux)
+			if err != nil {
+				return nil, err
+			}
+			syms[k] = *pesymn
 		}
 	}
 	if naux != 0 {
@@ -105,6 +115,5 @@ func (f *File) COFFSymbolReadSectionDefAux(idx int) (*COFFSymbolAuxFormat5, erro
 	// `rv` value is left with a bunch of `undefined`s. So replace
 	// rv = (*COFFSymbolAuxFormat5)(unsafe.Pointer(pesymn))
 	// (an in memory remap) with the following copy.
-	rv = copyToAuxFormat5(pesymn)
-	return rv, nil
+	return copyToAuxFormat5(pesymn)
 }

91f1d44246b326ba7bf3d7022ed09a34ea3e7da3
Grant Nelson <grant.nelson@workiva.com>
2024-01-30 17:10:29 -0700

    Updated crypto natives

---
+++ compiler/natives/src/crypto/internal/boring/bcache/cache.go
@@ -0,0 +1,30 @@
+//go:build js
+// +build js
+
+package bcache
+
+import "unsafe"
+
+// Cache relies on GC to periodically clear the cache.
+// Since GopherJS doesn't have the same GC hooks, it currently can not
+// register this cache with the GC.
+// Without this cache Boring crypto, in particular public and private
+// RSA and ECDSA keys, will be slower because the cache will always miss.
+type Cache struct{}
+
+func (c *Cache) Register()                           {}
+func (c *Cache) Clear()                              {}
+func (c *Cache) Get(k unsafe.Pointer) unsafe.Pointer { return nil }
+func (c *Cache) Put(k, v unsafe.Pointer)             {}
+
+//gopherjs:purge
+func (c *Cache) table() *[cacheSize]unsafe.Pointer
+
+//gopherjs:purge
+type cacheEntry struct{}
+
+//gopherjs:purge
+func registerCache(unsafe.Pointer)
+
+//gopherjs:purge
+const cacheSize = 1021
--- compiler/natives/src/crypto/internal/nistec/nistec_test.go
+++ compiler/natives/src/crypto/internal/nistec/nistec_test.go
@@ -4,8 +4,10 @@
 package nistec_test
 
 import (
-	"crypto/elliptic"
 	"testing"
+
+	"crypto/elliptic"
+	"crypto/internal/nistec"
 )
 
 func TestAllocations(t *testing.T) {
@@ -31,7 +33,7 @@ func TestEquivalents(t *testing.T) {
 }
 
 //gopherjs:override-signature
-func testEquivalents(t *testing.T, newPoint, newGenerator func() WrappedPoint, c elliptic.Curve) {}
+func testEquivalents(t *testing.T, newPoint, newGenerator func() nistec.WrappedPoint, c elliptic.Curve)
 
 func TestScalarMult(t *testing.T) {
 	t.Run("P224", func(t *testing.T) {
@@ -49,14 +51,14 @@ func TestScalarMult(t *testing.T) {
 }
 
 //gopherjs:override-signature
-func testScalarMult(t *testing.T, newPoint, newGenerator func() WrappedPoint, c elliptic.Curve)
+func testScalarMult(t *testing.T, newPoint, newGenerator func() nistec.WrappedPoint, c elliptic.Curve)
 
 func BenchmarkScalarMult(b *testing.B) {
 	b.Run("P224", func(b *testing.B) {
 		benchmarkScalarMult(b, nistec.NewP224WrappedGenerator(), 28)
 	})
 	b.Run("P256", func(b *testing.B) {
-		benchmarkScalarMult(b, nistec.NewP256GWrappedenerator(), 32)
+		benchmarkScalarMult(b, nistec.NewP256WrappedGenerator(), 32)
 	})
 	b.Run("P384", func(b *testing.B) {
 		benchmarkScalarMult(b, nistec.NewP384WrappedGenerator(), 48)
@@ -67,11 +69,11 @@ func BenchmarkScalarMult(b *testing.B) {
 }
 
 //gopherjs:override-signature
-func benchmarkScalarMult(b *testing.B, p WrappedPoint, scalarSize int)
+func benchmarkScalarMult(b *testing.B, p nistec.WrappedPoint, scalarSize int)
 
 func BenchmarkScalarBaseMult(b *testing.B) {
 	b.Run("P224", func(b *testing.B) {
-		benchmarkScalarBaseMult(b, nistec.NewP22Wrapped4Generator(), 28)
+		benchmarkScalarBaseMult(b, nistec.NewP224WrappedGenerator(), 28)
 	})
 	b.Run("P256", func(b *testing.B) {
 		benchmarkScalarBaseMult(b, nistec.NewP256WrappedGenerator(), 32)
@@ -80,9 +82,9 @@ func BenchmarkScalarBaseMult(b *testing.B) {
 		benchmarkScalarBaseMult(b, nistec.NewP384WrappedGenerator(), 48)
 	})
 	b.Run("P521", func(b *testing.B) {
-		benchmarkScalarBaseMult(b, nistec.NewP521GWrappedenerator(), 66)
+		benchmarkScalarBaseMult(b, nistec.NewP521WrappedGenerator(), 66)
 	})
 }
 
 //gopherjs:override-signature
-func benchmarkScalarBaseMult(b *testing.B, p WrappedPoint, scalarSize int)
+func benchmarkScalarBaseMult(b *testing.B, p nistec.WrappedPoint, scalarSize int)

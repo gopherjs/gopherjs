9caeb629cce7e990dceedf3c2468d59b2d05f41d
Grant Nelson <grant.nelson@workiva.com>
2024-01-30 17:52:05 -0700

    Random cleanup

--- compiler/natives/src/syscall/js/export_test.go
+++ compiler/natives/src/syscall/js/export_test.go
@@ -4,6 +4,5 @@
 package js
 
 // Defined to avoid a compile error in the original TestGarbageCollection()
-// body. Can't use gopherjs:prune-original on it, since it causes an unused
-// import error.
+// body.
 var JSGo Value

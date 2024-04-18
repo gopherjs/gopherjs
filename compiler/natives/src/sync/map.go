3ce79d7c49ca5572393ce285b568155ed43d80c2
Grant Nelson <grant.nelson@workiva.com>
2024-04-18 14:40:53 -0600

    Removing generics overrides

--- compiler/natives/src/sync/map.go
+++ compiler/natives/src/sync/map.go
@@ -6,7 +6,8 @@ package sync
 type Map struct {
 	mu Mutex
 
-	// replaced atomic.Pointer[readOnly] for go1.20 without generics.
+	// TODO(grantnelson-wf): Remove this override after generics are supported.
+	// https://github.com/gopherjs/gopherjs/issues/1013.
 	// This override is still needed with initial generics support because otherwise we get:
 	//	[compiler panic]  unexpected compiler panic while building package "reflect":
 	//	requesting ID of instance {type sync/atomic.Pointer[T any] struct{_ [0]*T; _ sync/atomic.noCopy; v unsafe.Pointer} sync.readOnly}

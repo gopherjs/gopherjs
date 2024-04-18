d3314f13fa7632f68cd96a669c9468acecc36959
Grant Nelson <grant.nelson@workiva.com>
2024-04-18 14:34:46 -0600

    Removing generics overrides

--- compiler/natives/src/sync/map.go
+++ compiler/natives/src/sync/map.go
@@ -7,6 +7,10 @@ type Map struct {
 	mu Mutex
 
 	// replaced atomic.Pointer[readOnly] for go1.20 without generics.
+	// This override is still needed with initial generics support because otherwise we get:
+	//	[compiler panic]  unexpected compiler panic while building package "reflect":
+	//	requesting ID of instance {type sync/atomic.Pointer[T any] struct{_ [0]*T; _ sync/atomic.noCopy; v unsafe.Pointer} sync.readOnly}
+	//	that hasn't been added to the set
 	read atomicReadOnlyPointer
 
 	dirty  map[any]*entry
@@ -19,30 +23,3 @@ type atomicReadOnlyPointer struct {
 
 func (x *atomicReadOnlyPointer) Load() *readOnly     { return x.v }
 func (x *atomicReadOnlyPointer) Store(val *readOnly) { x.v = val }
-
-type entry struct {
-
-	// replaced atomic.Pointer[any] for go1.20 without generics.
-	p atomicAnyPointer
-}
-
-type atomicAnyPointer struct {
-	v *any
-}
-
-func (x *atomicAnyPointer) Load() *any     { return x.v }
-func (x *atomicAnyPointer) Store(val *any) { x.v = val }
-
-func (x *atomicAnyPointer) Swap(new *any) *any {
-	old := x.v
-	x.v = new
-	return old
-}
-
-func (x *atomicAnyPointer) CompareAndSwap(old, new *any) bool {
-	if x.v == old {
-		x.v = new
-		return true
-	}
-	return false
-}

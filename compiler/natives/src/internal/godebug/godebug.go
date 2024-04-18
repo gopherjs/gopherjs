e09311719b163b8f2c9f01ec3569c2b27a511829
Grant Nelson <grant.nelson@workiva.com>
2024-04-18 13:37:48 -0600

    Updated godebug setUpdate

--- compiler/natives/src/internal/godebug/godebug.go
+++ compiler/natives/src/internal/godebug/godebug.go
@@ -37,6 +37,19 @@ func (s *Setting) Value() string {
 	return *s.value.Load()
 }
 
+// setUpdate is provided by package runtime.
+// It calls update(def, env), where def is the default GODEBUG setting
+// and env is the current value of the $GODEBUG environment variable.
+// After that first call, the runtime calls update(def, env)
+// again each time the environment variable changes
+// (due to use of os.Setenv, for example).
+//
+// GOPHERJS: For JS we currently will not be able
+// to access $GODEBUG via process.env nor watch
+// for changes via syscall.runtimeSetenv and
+// syscall.runtimeUnsetenv
+func setUpdate(update func(string, string)) {}
+
 func update(def, env string) {
 	updateMu.Lock()
 	defer updateMu.Unlock()

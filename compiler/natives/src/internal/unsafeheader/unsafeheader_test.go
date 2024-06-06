98d2501ad1b67edb9c11d63e75059f3ff574a9e9
Grant Nelson <grant.nelson@workiva.com>
2024-06-06 12:45:45 -0600

    Working on reflect and removing unsafeheader

--- compiler/natives/src/internal/unsafeheader/unsafeheader_test.go
+++ compiler/natives/src/internal/unsafeheader/unsafeheader_test.go
@@ -5,6 +5,16 @@ package unsafeheader_test
 
 import "testing"
 
+func TestTypeMatchesReflectType(t testing.T) {
+	t.Skip("GopherJS uses different slice and string implementation than internal/unsafeheader.")
+}
+
+//gopherjs:purge
+func testHeaderMatchesReflect()
+
+//gopherjs:purge
+func typeCompatible()
+
 func TestWriteThroughHeader(t *testing.T) {
 	t.Skip("GopherJS uses different slice and string implementation than internal/unsafeheader.")
 }

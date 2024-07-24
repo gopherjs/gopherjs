ba56db7ace6253966a00704d744597e35c89bd63
Grant Nelson <grant.nelson@workiva.com>
2024-07-24 13:22:50 -0600

    Removing generics from sync's map

--- compiler/natives/src/sync/map.go
+++ compiler/natives/src/sync/map.go
@@ -6,7 +6,7 @@ package sync
 type Map struct {
 	mu Mutex
 
-	// replaced atomic.Pointer[readOnly] for go1.20 without generics.
+	// replaced atomic.Pointer[readOnly] since GopherJS does not fully support generics for go1.20 yet.
 	read atomicReadOnlyPointer
 
 	dirty  map[any]*entry
@@ -19,30 +19,3 @@ type atomicReadOnlyPointer struct {
 
 func (x *atomicReadOnlyPointer) Load() *readOnly     { return x.v }
 func (x *atomicReadOnlyPointer) Store(val *readOnly) { x.v = val }
-
-type entry struct {
-
-	// replaced atomic.Pointer[any] for go1.20 without generics.
-	p atomicAnyPointer
-}
-
-type atomicAnyPointer struct {
-	v *any
-}
-
-func (x *atomicAnyPointer) Load() *any     { return x.v }
-func (x *atomicAnyPointer) Store(val *any) { x.v = val }
-
-func (x *atomicAnyPointer) Swap(new *any) *any {
-	old := x.v
-	x.v = new
-	return old
-}
-
-func (x *atomicAnyPointer) CompareAndSwap(old, new *any) bool {
-	if x.v == old {
-		x.v = new
-		return true
-	}
-	return false
-}

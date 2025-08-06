b450c3fd2ec2ade3b244e1bd807ae1b6b9400b00
Grant Nelson <grant.nelson@workiva.com>
2025-08-06 12:53:11 -0600

    Updating more src/internal

--- compiler/natives/src/internal/reflectlite/reflect_mirror_test.go
+++ compiler/natives/src/internal/reflectlite/reflect_mirror_test.go
@@ -1,12 +1,11 @@
 //go:build js
-// +build js
 
 package reflectlite_test
 
-import (
-	"testing"
-)
+import "testing"
 
+//gopherjs:replace
 func TestMirrorWithReflect(t *testing.T) {
+	// TODO(grantnelson-wf): Is this skip still necessary? We should get more information on this test.
 	t.Skip("TestMirrorWithReflect")
 }

c8e565b551a724957d6bb57129f0697b560ba752
Grant Nelson <grant.nelson@workiva.com>
2025-08-07 14:54:50 -0600

    Updating overrides

--- compiler/natives/src/net/http/transport_test.go
+++ compiler/natives/src/net/http/transport_test.go
@@ -1,14 +1,15 @@
 //go:build js
-// +build js
 
 package http_test
 
 import "testing"
 
+//gopherjs:replace
 func TestTransportPersistConnLeakNeverIdle(t *testing.T) {
 	t.Skip("test relied on runtime.SetFinalizer(), which is not supported by GopherJS.")
 }
 
+//gopherjs:replace
 func TestTransportPersistConnContextLeakMaxConnsPerHost(t *testing.T) {
 	t.Skip("test relied on runtime.SetFinalizer(), which is not supported by GopherJS.")
 }
--- compiler/natives/src/net/netip/netip_test.go
+++ compiler/natives/src/net/netip/netip_test.go
@@ -1,10 +1,10 @@
 //go:build js
-// +build js
 
 package netip_test
 
 import "testing"
 
+//gopherjs:replace
 func TestAddrStringAllocs(t *testing.T) {
 	t.Skip("testing.AllocsPerRun not supported in GopherJS")
 }

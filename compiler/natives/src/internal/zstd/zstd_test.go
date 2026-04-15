d1a48e9e2bbe70afe0521edbbbccbd8440667331
Grant Nelson <grant.nelson@workiva.com>
2026-04-15 14:03:36 -0700

    skipping tests in internal/zstd

---
+++ compiler/natives/src/internal/zstd/zstd_test.go
@@ -0,0 +1,14 @@
+package zstd
+
+import "testing"
+
+//gopehrjs:replace
+func TestAlloc(t *testing.T) {
+	t.Skip(`testing.AllocsPerRun not supported in GopherJS`)
+}
+
+//gopehrjs:replace
+func bigData(t testing.TB) []byte {
+	t.Skip(`test requires access to ../../testdata that is not avaiable in GopherJS`)
+	return nil
+}

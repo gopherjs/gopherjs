833265832158cfb2f53310023f6b1aa664b980e1
Grant Nelson <grant.nelson@workiva.com>
2026-04-16 15:11:16 -0700

    fixing encoding/binary

--- compiler/natives/src/encoding/binary/binary_test.go
+++ compiler/natives/src/encoding/binary/binary_test.go
@@ -9,7 +9,7 @@ import (
 //gopherjs:replace The original cast the uint32 into a *byte which doesn't work in JS
 func TestNativeEndian(t *testing.T) {
 	const val = 0x12345678
-	s := []byte{0x78, 0x56, 0x34, 0x12}
+	s := []byte{0x78, 0x56, 0x34, 0x12} // LittleEndian
 	if v := NativeEndian.Uint32(s); v != val {
 		t.Errorf("NativeEndian.Uint32 returned %#x, expected %#x", v, val)
 	}

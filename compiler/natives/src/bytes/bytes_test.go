3a82b6215d416c0aa70c04db6ea361a4076d489f
Grant Nelson <grant.nelson@workiva.com>
2026-04-23 16:07:47 -0600

    Fixed merge conflict

--- compiler/natives/src/bytes/bytes_test.go
+++ compiler/natives/src/bytes/bytes_test.go
@@ -9,8 +9,3 @@ func dangerousSlice(t *testing.T) []byte {
 
 	panic("unreachable")
 }
-
-//gopherjs:replace
-func TestIssue65571(t *testing.T) {
-	t.Skip("TestIssue65571 expects int to be greater than 32 bits")
-}

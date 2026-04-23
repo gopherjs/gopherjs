2370c4077e872a2e98e25d7e74f6a14aa20310f3
Grant Nelson <grant.nelson@workiva.com>
2026-04-23 14:05:50 -0600

    Added comment to WaitGroup counter not zero skipped tests

--- compiler/natives/src/log/log_test.go
+++ compiler/natives/src/log/log_test.go
@@ -1,13 +1,13 @@
 //go:build js
 
-package token
+package log
 
 import "testing"
 
-func TestFileSetRace(t *testing.T) {
+func TestOutputRace(t *testing.T) {
 	t.Skip("Fails with: WaitGroup counter not zero")
-	// "go/token" files are updated in build/build.go#augmentOriginalImports to
+	// "log" files are updated in build/build.go#augmentOriginalImports to
 	// use the `gopherjs/nosync` package instead of `sync`.
 	// `nosync`'s WaitGroup Wait method panics if the counter is not zero since it assumes
-	// the code will be synchronously executed for specific packages including go/token.
+	// the code will be synchronously executed for specific packages including log.
 }

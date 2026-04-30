6004861389b404f24f258032232996d9a497c46a
Grant Nelson <grant.nelson@workiva.com>
2026-04-30 10:37:01 -0600

    Added skip for wasmimport

--- compiler/natives/src/syscall/js/js_test.go
+++ compiler/natives/src/syscall/js/js_test.go
@@ -16,3 +16,17 @@ func TestIntConversion(t *testing.T) {
 func TestGarbageCollection(t *testing.T) {
 	t.Skip("GC is not supported by GopherJS")
 }
+
+// GopherJS does not currently support the //go:wasmimport directive
+// (see https://go.dev/blog/wasi and https://github.com/golang/go/issues/59149).
+// Functions declared with //go:wasmimport compile to "native function not
+// implemented" runtime stubs, which panic and crash the Node process before
+// the testing framework can recover. Skip the affected tests until proper
+// support is added; they cannot be overridden without defeating the test.
+func TestWasmImport(t *testing.T) {
+	t.Skip("//go:wasmimport is not supported by GopherJS")
+}
+
+func TestWasmExport(t *testing.T) {
+	t.Skip("//go:wasmimport is not supported by GopherJS")
+}

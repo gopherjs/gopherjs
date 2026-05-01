2e2cf02d67d5d4f42a43e394d8af35c3dc4cc023
Grant Nelson <grant.nelson@workiva.com>
2026-05-01 14:12:05 -0600

    Updating more native overrides for go1.21

--- compiler/natives/src/syscall/js/js_test.go
+++ compiler/natives/src/syscall/js/js_test.go
@@ -16,3 +16,15 @@ func TestIntConversion(t *testing.T) {
 func TestGarbageCollection(t *testing.T) {
 	t.Skip("GC is not supported by GopherJS")
 }
+
+// GopherJS does not currently support the //go:wasmimport directive
+// (see https://go.dev/blog/wasi and https://github.com/golang/go/issues/59149).
+// Functions declared with //go:wasmimport compile to "native function not
+// implemented" runtime stubs, which panic and crash the Node process before
+// the testing framework can recover. Skip the affected tests until proper
+// support is added; they cannot be overridden without defeating the test.
+//
+//gopherjs:replace
+func TestWasmImport(t *testing.T) {
+	t.Skip("//go:wasmimport is not supported by GopherJS")
+}
